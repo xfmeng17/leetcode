aa3817558253ca836280ce0faa76a2942d2f9f94
xfmeng17 <xfmeng17@foxmail.com>
2018-10-18 12:01:11 +0800

    finish 657 Robot Return to Origin and 832 Flipping an image

---
+++ c++/832.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+    	for (vector<int> &r : A) {
+    		int size = r.size();
+    		for (int i = 0; i < size / 2; i++) {
+    			int temp = r[i];
+    			r[i] = r[size - 1 - i] ^ 1;
+    			r[size - 1 - i] = temp ^ 1;
+    		}
+    		if (size % 2 == 1) {
+    			r[size / 2] ^= 1;
+    		}
+    	}
+    	return A;
+    }
+};
