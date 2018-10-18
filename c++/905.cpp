90372836fe701cdc5439e5b09b5ddf2e565ebd08
eggxfmeng <xfmeng17@foxmail.com>
2018-10-18 11:16:43 +0800

    finish 905 Sort Array By Parity

---
+++ c++/905.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+    	int i = 0;
+    	int j = A.size() - 1;
+    	while (i < j) {
+    		while (i < j && A[i] % 2 == 0) { i++; }
+    		while (i < j && A[j] % 2 == 1) { j--; }
+    		if (i < j) {
+    			int temp = A[i];
+    			A[i] = A[j];
+    			A[j] = temp;
+    			i++;
+    			j--;
+    		}
+    	}
+    	return A;
+    }
+};
