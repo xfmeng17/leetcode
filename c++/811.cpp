cb6b9e85e98448d748543d4c6c36cb218d9916ee
xfmeng17 <xfmeng17@foxmail.com>
2018-10-31 19:07:49 +0800

    update

--- c++/811.cpp
+++ c++/811.cpp
@@ -3,10 +3,24 @@ public:
     vector<string> subdomainVisits(vector<string>& cpdomains) {
     	vector<string> res;
         unordered_map<string, int> set;
-        vector<string> subdomins = split(cpdomains[0], ' ');
-        // for (int i = 0; i < cpdomains.size(); i++) {
-        // 	cout << cpdomains[i] << endl;
-        // }
+        for (int i = 0; i < cpdomains.size(); i++) {
+        	vector<string> vec_record = split(cpdomains[i], ' ');
+        	int count = atoi(vec_record[0].c_str());
+        	vector<string> vec_domain =  subdomain(vec_record[1], '.');
+        	for (int j = 0; j < vec_domain.size(); j++) {
+        		if (set.count(vec_domain[i]) <= 0) {
+	        		set.insert({vec_domain[i], count});
+	        	} else {
+	        		set[vec_domain[i]] = set[vec_domain[i]] + count;
+	        	}
+        	}
+        	
+        }
+        for (auto i = set.begin(); i != set.end(); i++) {
+        	string str = to_string(i->second) + " " + i->first;
+        	res.push_back(str);
+        }
+
         return res;
     }
 
@@ -25,9 +39,23 @@ public:
     	}
     	res.push_back(str.substr(pos - len));
 
-    	for (int i = 0; i < res.size(); i++) {
-    		cout << res[i] << endl;
+    	return res;
+    }
+    vector<string> subdomain(string domain, char del) {
+    	vector<string> res;
+    	int pos = 0;
+    	res.push_back(domain);
+    	while (pos < domain.length()) {
+    		if (domain[pos] == del) {
+    			domain = substr(pos + 1);
+    			res.push_back(domain);
+    			pos = 0;
+    		}
     	}
+    	if (domain != "") {
+    		res.push_back(domain)
+    	}
+    	
     	return res;
     }
 };
