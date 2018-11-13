21f9ab5c699cd4b5619745161437e6a2cd045b12
eggxfmeng <xfmeng17@foxmail.com>
2018-11-13 10:31:49 +0800

    unfinish 892 and finish 860 Lemonade Change

--- c++/892.cpp
+++ c++/892.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int surfaceArea(vector<vector<int>>& grid) {
-    	int res = 0;
-        for (int i = 0; i < grid.size(); i++) {
-        	for (int j = 0; j < grid[i].size(); j++) {
-        		int s = gird[i][j] > 0 ? 2 : 0;
-        		if (i - 1 < 0 || grid[i-1][j] < grid[i][j]) s += grid[i][j];
-        		if (i + 1 >= grid.size() || grid[i+1][j] < grid[i][j]) s += grid[i][j];
-        		if (j - 1 < 0 || grid[i][j-1] < grid[i][j]) s += grid[i][j];
-        		if (j + 1 >= grid[i].size() || grid[i][j+1] < grid[i][j]) s += grid[i][j];
-        		res += s;
-        	}
-        }
-        return res;
+    	vector<int> x;
+    	vector<int> y;
+    	int z = 0;
+    	int height = grid.size();
+    	int width = grid[0].size();
+
+    	for (int i = 0; i < height; i++) {
+    		int max = 0;
+    		for (int j = 0; j < width; j++) {
+    			if (grid[i][j] > max) {
+    				max = grid[i][j];
+    			}
+    		}
+    		x.push_back(max);
+    	}
+    	for (int j = 0; j < width; j++) {
+    		int max = 0;
+    		for (int i = 0; i < height; i++) {
+				if (grid[i][j] > max) {
+					max = grid[i][j];
+				}
+    		}
+    	}
     }
 };
