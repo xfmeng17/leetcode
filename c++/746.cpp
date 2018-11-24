41b8614e44b9eb237fd3e362c81a74d21a4ba54d
xfmeng17 <xfmeng17@foxmail.com>
2018-11-24 13:46:49 +0800

    finish 746 Min Cost Climbing Stairs but need a whole dp solution

--- c++/746.cpp
+++ c++/746.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-    	return func1(cost, 0);   
+    	unordered_map<int, int> dp;
+    	return min(func1(cost, 0, dp), func1(cost, 1, dp));   
     }
-	
-	int func1(vector<int>& cost, int i) {
+
+	// ** don't store result into dp will cause TLE.
+	int func1(vector<int>& cost, int i, unordered_map<int, int>& dp) {
+		if (dp[i]) {
+			return dp[i];
+		}
 		if (i >= cost.size()) {
 			return 0;
 		}
-		if (i == cost.size() - 1) {
-			return cost[i];
-		}
-		int one = cost[i] + func1(cost, i + 1);
-		int two = one;
-		if (i < cost.size() - 2) {
-			two = cost[i + 1] + func1(cost, i + 2);
-		}
-		return min(one, two);
+		int one = cost[i] + func1(cost, i + 1, dp);
+		int two = cost[i] + func1(cost, i + 2, dp);
+		dp[i] = min(one, two);
+		return dp[i];
+	}
+	// ** I think there are another approach let down to top
+	int func2() {
+		return 0;
 	}    
 };
