dcb55c269ad0f3a36165f30da428e2fafa439989
xfmeng17 <xfmeng17@foxmail.com>
2018-11-24 14:19:59 +0800

    finish 892 Surface Area of 3D Shapes

--- c++/892.cpp
+++ c++/892.cpp
@@ -1,28 +1,60 @@
 class Solution {
 public:
     int surfaceArea(vector<vector<int>>& grid) {
-    	vector<int> x;
-    	vector<int> y;
-    	int z = 0;
-    	int height = grid.size();
-    	int width = grid[0].size();
+        // return func1(grid);
+        return func2(grid);
+    }
 
-    	for (int i = 0; i < height; i++) {
-    		int max = 0;
-    		for (int j = 0; j < width; j++) {
-    			if (grid[i][j] > max) {
-    				max = grid[i][j];
-    			}
-    		}
-    		x.push_back(max);
-    	}
-    	for (int j = 0; j < width; j++) {
-    		int max = 0;
-    		for (int i = 0; i < height; i++) {
-				if (grid[i][j] > max) {
-					max = grid[i][j];
-				}
-    		}
-    	}
+    int func1(vector<vector<int>>& grid) {
+        int all = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                all += grid[i][j];
+            }
+        }
+        all = all * 6;
+
+        int self = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                self += grid[i][j] > 0 ? grid[i][j] - 1 : 0;
+            }
+        }
+        self = self * 2;
+
+        int each = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (j + 1 < grid[i].size()) {
+                    each += min(grid[i][j], grid[i][j+1]);
+                }
+                if (i + 1 < grid.size()) {
+                    each += min(grid[i][j], grid[i+1][j]);
+                }
+            }
+        }
+        each = each * 2;
+
+        return all - self -each;
+    }
+
+    // ** like func1 just put all test in one for
+    int func2(vector<vector<int>>& grid) {
+        int all = 0;
+        int self = 0;
+        int each = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                all += grid[i][j];
+                self += grid[i][j] > 0 ? grid[i][j] - 1 : 0;
+                if (j + 1 < grid[i].size()) {
+                    each += min(grid[i][j], grid[i][j+1]);
+                }
+                if (i + 1 < grid.size()) {
+                    each += min(grid[i][j], grid[i+1][j]);
+                }
+            }
+        }
+        return all * 6 - self * 2 - each * 2;
     }
 };
