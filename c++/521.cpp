24c83a47887b527c571029144dd9680f7f0a86dd
xfmeng17 <xfmeng17@foxmail.com>
2018-12-01 17:38:59 +0800

    finish 521 Longest Uncommon Subsequence I, this is a silly question.

--- c++/521.cpp
+++ c++/521.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        
-    }
-
-    // ** straight forward
-    int func1(string a, string b) {
-    	if (b.find(a)) {
-    		return a.length();
+    	if (a.compare(b) != 0) {
+    		return max(a.length(), b.length());
     	}
-    	
+    	return -1;
     }
 };
