f27dab18540ab6ae54d0e7be363bf3853480fa93
xfmeng17 <xfmeng17@foxmail.com>
2018-12-02 16:49:47 +0800

    finish 812 Largest Triangle Area with QingJiuZhao's formula

--- c++/812.cpp
+++ c++/812.cpp
@@ -1,28 +1,71 @@
 class Solution {
 public:
     double largestTriangleArea(vector<vector<int>>& points) {
-    	return func1(points);   
+    	// return func1(points);  
+    	return func2(points); 
     }
-    // ** straight forward with QingJiuZhao's formula
+
+    // ** straight forward with Heron's formula
+    // ** Time(N^3)
+    // ** Wrong Answer, I think it exists some mathematical precision problem.
+
+    /**********
+    Heron's formula:
+    A = sqrt ( s * (s - a) * (s - b) * (s - c) ) and s = (a + b + c) / 2
+    **********/
+
     double func1(vector<vector<int>>& points) {
-    	vector<int> edge;
+    	double res = 0.0;
     	for (int i = 0; i < points.size(); i++) {
     		for (int j = i + 1; j < points.size(); j++) {
-    			int x = points[i][0] - points[j][0];
-    			int y = points[i][1] - points[j][1];
-    			edge.push_back(x * x + y * y);
+    			for (int k = j + 1; k < points.size(); k++) {
+    				double a = sqrt(pow((points[i][0] - points[j][0]), 2) + 
+    					pow((points[i][1] - points[j][1]), 2));
+    				double b = sqrt(pow((points[i][0] - points[k][0]), 2) + 
+    					pow((points[i][1] - points[k][1]), 2));
+    				double c = sqrt(pow((points[j][0] - points[k][0]), 2) + 
+    					pow((points[j][1] - points[k][1]), 2));
+    				double s = (a + b + c) / 2.0;
+    				double area = sqrt(s * (s - a) * (s - b) * (s - c));
+    				res = max(area, res);
+    			}
     		}
     	}
-    	sort(edge.begin(), edge.end(), myCompare);
-    	int a = edge[0];
-    	int b = edge[1];
-    	int c = edge[2];
-    	cout << "a = " << a << endl;
-    	cout << "b = " << b << endl;
-    	cout << "c = " << c << endl;
-    	return sqrt((a * c - pow((a + c - b) / 2, 2)) / 4.0);
+    	return res;
 	}
-	static bool myCompare(int i, int j) {
-    	return i > j;
-    }
+
+	// ** Time(N^3)
+	/********** QingJiuZhao's formula:
+	A = sqrt ( (1.0/4.0) * [ (a2 * c2 - (a2 + c2 - b2)/2.0 * (a2 + c2 - b2)/2.0 ] )
+	and a2 = a * a, same b2 ,c2.
+	**********/
+
+	double func2(vector<vector<int>>& points) {
+    	double res = 0.0;
+    	for (int i = 0; i < points.size(); i++) {
+    		for (int j = i + 1; j < points.size(); j++) {
+    			for (int k = j + 1; k < points.size(); k++) {
+    				double a = pow((points[i][0] - points[j][0]), 2) + 
+    					pow((points[i][1] - points[j][1]), 2);
+    				double b = pow((points[i][0] - points[k][0]), 2) + 
+    					pow((points[i][1] - points[k][1]), 2);
+    				double c = pow((points[j][0] - points[k][0]), 2) + 
+    					pow((points[j][1] - points[k][1]), 2);
+    				double A = max(max(a, b), max(b, c));
+    				double C = min(min(a, b), min(b, c));
+    				double B = 0.0;
+    				if (A == b) {
+    					B = max(a, c);
+    				} else if (C == b) {
+    					B = min(a, c);
+    				} else {
+    					B = b;
+    				}
+    				double area = sqrt((1.0/4.0) * ((A * C) - pow((A + C - B) / 2.0, 2)));
+    				res = max(area, res);
+    			}
+    		}
+    	}
+    	return res;
+	}
 };
