4cf0fe0223644aa7b0994ea5393025e38bcaea82
xfmeng17 <xfmeng17@foxmail.com>
2018-12-21 16:21:16 +0800

    finish 814 Binary Tree Pruning

--- c++/814.cpp
+++ c++/814.cpp
@@ -10,6 +10,29 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        
+        return func1(root);
+    }
+
+    TreeNode* func1(TreeNode* root) {
+        if (root == NULL) {
+        	return NULL;
+        }
+
+        TreeNode* lf = func1(root->left);
+        TreeNode* rt = func1(root->right);
+
+        if (lf == NULL && rt == NULL) {
+        	if (root->val == 1) {
+        		root->left = NULL;
+        		root->right = NULL;
+        		return root;
+        	} else {
+        		return NULL;
+        	}
+        } else {
+        	root->left = lf;
+        	root->right = rt;
+        	return root;
+        }
     }
 };
