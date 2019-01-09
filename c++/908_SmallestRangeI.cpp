1f9ff26a3e1f013f43788e250e8f31c91c0207f8
xfmeng17 <xfmeng17@foxmail.com>
2019-01-09 21:36:12 +0800

    rename file names with problem title

---
+++ c++/908_SmallestRangeI.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+     	int max = A[0];
+     	int min = A[0];
+     	for (int a : A) {
+     		if (a > max) max = a;
+     		if (a < min) min = a;
+     	}
+     	return max - min - 2*K > 0 ? max - min - 2*K : 0;
+    }
+};
