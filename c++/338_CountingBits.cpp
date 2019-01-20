d3ca36b1570d36f5cdef2d4ab8619bcf7a590ecb
xfmeng17 <xfmeng17@foxmail.com>
2019-01-20 23:27:09 +0800

    Update: add a O(n) solution

--- c++/338_CountingBits.cpp
+++ c++/338_CountingBits.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        return func1(num);
+        // return func1(num);
+        return func2(num);
     }
 
     // ** easy one, O(n * sizeof(integer))
@@ -16,6 +17,24 @@ public:
         		cur = cur >> 1;
         	}
         	res.push_back(cnt);
+        }
+
+        return res;
+    }
+    vector<int> func2(int num) {
+        vector<int> res;
+        if (num < 0) { return res; }
+        
+        res.resize(num + 1);
+        res.push_back(0);
+        int e = 1;
+        for (int i = 1; i <= num; i++) {
+            if (i == e) {
+                res.push_back(1);
+                e *= 2;
+            } else {
+                res.push_back(res[e/2] + res[i % (e/2)]);
+            }
         }
 
         return res;
