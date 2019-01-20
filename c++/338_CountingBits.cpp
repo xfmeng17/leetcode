76a46fc034c0904ea6fc31d4a4372c2daa897c25
xfmeng17 <xfmeng17@foxmail.com>
2019-01-20 22:16:00 +0800

    Add: easy solution for 338 counting bits

---
+++ c++/338_CountingBits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        return func1(num);
+    }
+
+    // ** easy one, O(n * sizeof(integer))
+    vector<int> func1(int num) {
+    	vector<int> res;
+
+        for (int i = 0; i <= num; i++) {
+        	int cur = i;
+        	int cnt = 0;
+        	while (cur > 0) {
+        		cnt += cur & 1;
+        		cur >> 1;
+        	}
+        	res.push_back(cnt);
+        }
+
+        return res;
+    }
+};
