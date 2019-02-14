a51e28c584f9bdee5400f8d5c14357ab343f655b
xfmeng17 <xfmeng17@foxmail.com>
2019-02-14 10:33:33 +0800

    Add: init 983 minimum cost for tickets

---
+++ c++/983_MinimumCostForTickets.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        return func1(days, costs);        
+    }
+    int func1(vector<int>& days, vector<int>& costs) {
+        int res = 0;
+        return res;
+    }
+};
