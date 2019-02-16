5fcfe933aa6a78b87ab4a6c852bf672de76727e3
xfmeng17 <xfmeng17@foxmail.com>
2019-02-16 17:26:42 +0800

    Update: use straight forward dp by reference with memory trim to 2-rows

--- c++/712_MinimumAsciiDeleteSumOfTwoStrings.cpp
+++ c++/712_MinimumAsciiDeleteSumOfTwoStrings.cpp
@@ -5,11 +5,12 @@ public:
 	const int LEFT = 3;
 
     int minimumDeleteSum(string s1, string s2) {  
-    	return func1(s1, s2);      
+    	// return func1(s1, s2);    
+    	// return func2(s1, s2);  
+    	return func3(s1, s2);
     }
     // ** LCS with a extra trace table named 'b'
     // ** O(m*n) space and Memory Limit Exceeded
-
     int func1(string s1, string s2) {
     	int m = s1.length();
     	int n = s2.length();
@@ -78,7 +79,9 @@ public:
     	return s1AsciiSum + s2AsciiSum - maxRemainSum;
     	*/
     }
-    int maxPathAsciiSum(vector<vector<int>>& c, vector<vector<int>>& b, int i, int j, string s1, string s2) {
+    int maxPathAsciiSum(vector<vector<int>>& c, vector<vector<int>>& b,
+    	int i, int j, string s1, string s2) {
+
     	if (i <= 0 || j <= 0) {
     		return 0;
     	}
@@ -102,4 +105,61 @@ public:
     	return max(max(upLeft, up), left);
     }
 
+    // ** straight forward dp
+    /** reference:
+	dp[i][j] is the minimum cost for s1.substr(0, i) and s2.substr(0, j)
+    base dp[0][0] = 0, target dp[m][n], formular:
+    1. s1[i-1] == s2[j-1], dp[i][j] = dp[i-1][j-1], no deletion.
+    2. s1[i-1] != s2[j-1], dp[i][j] = min(dp[i][j-1], dp[i-1][j-1]),
+    delete either s1[i-1] or s2[j-1].
+    */
+    int func2(string s1, string s2) {
+    	int m = s1.length();
+    	int n = s2.length();
+    	vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    	for (int i = 1; i <= m; i++) {
+    		dp[i][0] = dp[i-1][0] + s1[i-1];
+    	}
+    	for (int j = 1; j <= n; j++) {
+    		dp[0][j] = dp[0][j-1] + s2[j-1];
+    	}
+
+    	for (int i = 1; i <= m; i++) {
+    		for (int j = 1; j <= n; j++) {
+    			if (s1[i-1] == s2[j-1]) {
+    				dp[i][j] = dp[i-1][j-1];
+    			} else {
+    				dp[i][j] = min(dp[i-1][j] + s1[i-1], dp[i][j-1] + s2[j-1]);
+    			}
+    		}
+    	}
+
+    	return dp[m][n];
+    }
+
+    // ** trim dp memory, 2 row
+    int func3(string s1, string s2) {
+    	int m = s1.length();
+    	int n = s2.length();
+    	vector<int> dp1(n + 1, 0);
+    	vector<int> dp2(n + 1, 0);
+    	for (int j = 1; j <= n; j++) {
+    		dp1[j] = dp1[j-1] + s2[j-1];
+    		dp2[j] = dp2[j-1] + s2[j-1];
+    	}
+    	for (int i = 1; i <= m; i++) {
+    		dp2[0] = dp1[0] + s1[i-1];
+    		for (int j = 1; j <= n; j++) {
+    			if (s1[i-1] == s2[j-1]) {
+    				dp2[j] = dp1[j-1];
+    			} else {
+    				dp2[j] = min(dp1[j] + s1[i-1], dp2[j-1] + s2[j-1]);
+    			}
+    		}
+    		for (int j = 0; j <= n; j++) {
+    			dp1[j] = dp2[j];
+    		}
+    	}
+    	return dp2[n];
+    }
 };
