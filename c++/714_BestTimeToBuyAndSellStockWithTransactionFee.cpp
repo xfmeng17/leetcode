f6ccb445d43582905469dc204200cecde2cb0938
xfmeng17 <xfmeng17@foxmail.com>
2019-02-19 17:20:30 +0800

    Update: use recursion+memo top-down with TLE

--- c++/714_BestTimeToBuyAndSellStockWithTransactionFee.cpp
+++ c++/714_BestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-     	return func1(prices, fee);   
+     	// return func1(prices, fee);
+     	return func2(prices, fee);
     }
 
     // ** 1. recursion, top-down, time-limit-exceeded
@@ -29,5 +30,34 @@ public:
     	return ret;
     }
 
-    // ** 2. 
+    // ** 2. recursion+memo, top-down
+    int func2(vector<int>& prices, int fee) {
+    	int n = prices.size();
+    	vector<vector<int>> memo(n, vector<int>(n, -1));
+    	return helper2(prices, fee, 0, n-1, memo);
+    }
+    int helper2(vector<int>& prices, int fee, int lo, int hi, vector<vector<int>>& memo) {
+    	if (hi <= lo) {
+    		return 0;
+    	}
+    	if (hi - lo == 1) {
+    		memo[lo][hi] = max(0, prices[hi] - prices[lo] - fee);
+    		return memo[lo][hi];
+    	}
+    	if (memo[lo][hi] >= 0) {
+    		return memo[lo][hi];
+    	}
+
+    	memo[lo][hi] = 0;
+    	for (int i = lo; i < hi; i++) {
+    		for (int j = i+1; j <= hi; j++) {
+    			int p = prices[j] - prices[i] - fee;
+    			int lf = helper2(prices, fee, lo, i-1, memo);
+    			int rt = helper2(prices, fee, j+1, hi, memo);
+    			memo[lo][hi] = max(memo[lo][hi], lf + p + rt);
+    		}
+    	}
+
+    	return memo[lo][hi];
+    }
 };
