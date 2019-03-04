9e89685b5ea77c6f56b30fafea392fa7b680f0f4
xfmeng17 <xfmeng17@foxmail.com>
2019-03-04 17:12:59 +0800

    Add: finish 392 is subsequence with 2 slow solutions

---
+++ c++/392_IsSubsequence.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // return func1(s, t);
+        return func2(s, t);
+    }
+
+    // ** slow, maybe dp is not a good idea
+    bool func1(string s, string t) {
+        if (s.length() <= 0) {
+            return true;
+        }
+
+        vector<int> dp(s.length() + 1, 0);
+        int pre = 0;
+        for (int i = 0; i < t.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                pre = dp[j+1];
+                if (s[j] == t[i]) {
+                    dp[j+1] = pre + 1;
+                } else {
+                    dp[j+1] = max(dp[j], dp[j+1]);
+                }
+                if (dp[j+1] == s.length()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // ** iterative check
+    bool func2(string s, string t) {
+        int si = 0;
+        int ti = 0;
+
+        for (si = 0; si < s.length(); si++) {
+            while (ti < t.length() && t[ti] != s[si]) {
+                ti++;
+            }
+            if (ti >= t.length()) {
+                return false;
+            } else {
+                ti++;
+            }
+        }
+
+        return true;
+    }
+
+};
