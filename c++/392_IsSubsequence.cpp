9d816cf5efdcf3a2b1c25ab15b104388f102927d
xfmeng17 <xfmeng17@foxmail.com>
2019-03-05 16:04:13 +0800

    Update: 392 add a brilliant sulotion base on binary-search from discuss

--- c++/392_IsSubsequence.cpp
+++ c++/392_IsSubsequence.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         // return func1(s, t);
-        return func2(s, t);
+        // return func2(s, t);
+        return func3(s, t);
     }
 
     // ** slow, maybe dp is not a good idea
@@ -48,5 +49,43 @@ public:
 
         return true;
     }
+    
+    // ** reference binary search
+    bool func3(string s, string t) {
+        vector<vector<int>> set(256);
+        for (int i = 0; i < t.length(); i++) {
+            set[t[i]].push_back(i);
+        }
 
+        int prev = -1;
+        for (auto c : s) {
+            vector<int>& vec = set[c];
+            if (vec.size() <= 0) {
+                return false;
+            }
+            int pos = upperBound(vec, prev);
+            if (pos == vec.size()) {
+                return false;
+            }
+            prev = vec[pos];
+        }
+
+        return true;
+    }
+
+    int upperBound(vector<int>& vec, int target) {
+        int lo = 0;
+        int hi = vec.size() - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (vec[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
 };
