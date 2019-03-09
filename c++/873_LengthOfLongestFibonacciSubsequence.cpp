ad6aca861959c2815299a64e3e92d94486b360f3
xfmeng17 <xfmeng17@foxmail.com>
2019-03-09 12:17:30 +0800

    feat(873): 1st try 873 with TLE
    Problem: 873 Length of longest fibonacci subsequence.
    
    Use directly top-down recursive approach and,
    of cause, get TLE.
    
    And for now, I'm practicing formatting commit message.

---
+++ c++/873_LengthOfLongestFibonacciSubsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+    	return func1(A);
+    }
+
+    // ** recursion top-down
+    int func1(vector<int>& A) {
+    	int ret = helper1(A, 0, 0, 0);
+
+    	return ret >= 3 ? ret : 0;
+    }
+    int helper1(vector<int>& A, int i, int prev1, int prev2) {
+    	if (i >= A.size()) {
+    		return 0;
+    	}
+
+    	if (prev1 == 0 || prev2 == 0) {
+    		int pick = 1 + helper1(A, i+1, prev2, A[i]);
+    		int notp = 0 + helper1(A, i+1, prev1, prev2);
+    		return max(pick, notp);
+    	}
+
+    	int notp = 0 + helper1(A, i+1, prev1, prev2);
+    	int pick = notp;
+    	if (prev1 + prev2 == A[i]) {
+    		pick = 1 + helper1(A, i+1, prev2, A[i]);
+    	}
+
+    	return max(pick, notp);
+    }
+};
