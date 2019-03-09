dbcf11d9cb2c4fe53074c5a0e796a01dff54e4bb
xfmeng17 <xfmeng17@foxmail.com>
2019-03-09 11:49:10 +0800

    feat(64): third try 64 with AC
    Use real bottom-up dp.
    
    And for now, I'm practicing formatting comment message.

--- c++/64_MinimumPathSum.cpp
+++ c++/64_MinimumPathSum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        return func1(grid);        
+        // return func1(grid);
+        // return func2(grid);   
+        return func3(grid);    
     }
     // ** top-down recursion
     // ** of cause, time-limit-exceeded
@@ -39,5 +41,77 @@ public:
         int down = grid[i][j] + helper1(grid, ht, wt, i, j+1, dsum);
 
         return min(right, down);
+    }
+
+    // ** recursion with memo
+    int func2(vector<vector<int>>& grid) {
+        int height = grid.size();
+        if (height <= 0) {
+            return 0;
+        }
+        int weight = grid[0].size();
+        if (weight <= 0) {
+            return 0;
+        }
+        
+        int dummySum = 0;
+        for (int j = 0; j < weight; j++) {
+            dummySum += grid[0][j];
+        }
+        for (int i = 1; i < height; i++) {
+            dummySum += grid[i][weight-1];
+        }
+        vector<vector<int>> memo(height, vector<int>(weight, 0));
+
+        return min(dummySum, helper2(grid, height, weight, 0, 0, memo, dummySum));
+    }
+    int helper2(vector<vector<int>>& grid,
+        int ht, int wt, int i, int j,
+        vector<vector<int>>& memo, int dsum) {
+        
+        if (i == ht - 1 && j == wt - 1) {
+            return grid[i][j];
+        }
+        if (i >= ht || j >= wt) {
+            return dsum;
+        }
+        if (memo[i][j]) {
+            return memo[i][j];
+        }
+
+        int right = grid[i][j] + helper2(grid, ht, wt, i+1, j, memo, dsum);
+        int down = grid[i][j] + helper2(grid, ht, wt, i, j+1, memo, dsum);
+
+        memo[i][j] = min(right, down);
+        return memo[i][j];
+    }
+
+    // ** real buttom-up dp
+    int func3(vector<vector<int>>& grid) {
+        int height = grid.size();
+        if (height <= 0) {
+            return 0;
+        }
+        int weight = grid[0].size();
+        if (weight <= 0) {
+            return 0;
+        }
+        
+        vector<int> dp(weight + 1, 0);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < weight; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j+1] = grid[i][j];
+                } else if (i == 0) {
+                    dp[j+1] = grid[i][j] + dp[j];
+                } else if (j == 0) {
+                    dp[j+1] = grid[i][j] + dp[j+1];
+                } else {
+                    dp[j+1] = grid[i][j]+ min(dp[j], dp[j+1]);
+                }
+            }
+        }
+
+        return dp[weight];
     }
 };
