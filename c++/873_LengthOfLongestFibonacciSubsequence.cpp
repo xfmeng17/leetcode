8c0a95516afaa516ce5a7332c78ff5785ce3667d
xfmeng17 <xfmeng17@foxmail.com>
2019-03-10 13:51:25 +0800

    feat(873): 5th, dp refer to discuss, AC
    dp[a, b] means the length of fibo sequence ends up
    with (a, b), then the formula is: dp[a, b] = dp[b-a, a] + 1 or 2.
    
    The complexity reduce to O(N^2).
    
    Yes, I'm practicing formatting commit message.

--- c++/873_LengthOfLongestFibonacciSubsequence.cpp
+++ c++/873_LengthOfLongestFibonacciSubsequence.cpp
@@ -4,7 +4,8 @@ public:
     	// return func1(A);
     	// return func2(A);
     	// return func3(A);
-    	return func4(A);
+    	// return func4(A);
+    	return func5(A);
     }
 
     // ** recursion top-down
@@ -35,8 +36,38 @@ public:
 
     // ** recursion top-down + memo
     // ** can't figure out, I give it up 0.0
+    // ** update 190310, such memoization don't make any effect.
     int func2(vector<int>& A) {
-    	return 0;
+    	vector<vector<int>> memo(A.size()+1, vector<int>(A.size()+1, 0));
+    	memo[0][0] = 0;
+    	int ret = helper2(A, 1, 0, 0, memo);
+    	return ret >= 3 ? ret : 0;
+    }
+    int helper2(vector<int>& A, int i, int p1, int p2,
+    	vector<vector<int>>& memo) {
+
+    	if (i > A.size()) {
+    		return 0;
+    	}
+    	if (memo[p1][p2] > 0) {
+    		return memo[p1][p2];
+    	}
+
+    	if (p1 == 0 || p2 == 0) {
+    		int pick = 1 + helper2(A, i+1, p2, i, memo);
+    		memo[p2][i] = pick;
+    		int notp = 0 + helper2(A, i+1, p1, p2, memo);
+    		return max(pick, notp);
+    	}
+
+    	int notp = 0 + helper2(A, i+1, p1, p2, memo);
+    	int pick = notp;
+    	if (A[p1-1] + A[p2-1] == A[i-1]) {
+    		pick = 1 + helper2(A, i+1, p2, i, memo);
+    		memo[p2][i] = pick;
+    	}
+
+    	return max(pick, notp);
     }
 
     // ** LCS between origin input and fibonacii sequence
@@ -94,6 +125,24 @@ public:
     				len++;
     			}
     			ret = max(ret, len);
+    		}
+    	}
+    	return ret > 2 ? ret : 0;
+    }
+
+    // ** dp[i, j] meams the maximum length of subsequence
+    // ** ends with A[i] and A[j].
+    // ** Implement refer to discuss, brilliant code.
+    int func5(vector<int>& A) {
+    	unordered_map<int, int> map;
+    	int n = A.size(), ret = 0;
+    	vector<vector<int>> dp(n, vector<int>(n, 0));
+    	for (int j = 0; j < n; j++) {
+    		map[A[j]] = j;
+    		for (int i = 0; i < j; ++i) {
+    			int k = map.find(A[j] - A[i]) == map.end() ? -1 : map[A[j] - A[i]];
+    			dp[i][j] = (A[j] - A[i] < A[i] && k >= 0) ? dp[k][i] + 1 : 2;
+    			ret = max(ret, dp[i][j]);
     		}
     	}
     	return ret > 2 ? ret : 0;
