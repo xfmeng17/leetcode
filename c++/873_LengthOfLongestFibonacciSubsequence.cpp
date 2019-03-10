f1ec2f09b25c3a1c7c2a7a03af916db109576faa
xfmeng17 <xfmeng17@foxmail.com>
2019-03-10 11:30:16 +0800

    feat(873): 3rd, LCS + finonacii sequence, wrong thought
    Totally wrong, eg, input [1, 3, 5] is not fibonacii-like
    but it is a subsequence of [1, 2, 3, 5, 8...].
    
    Formula should be, subsequence of input array and substring
    of fibonacii array.
    
    Yes, I'm practicing formatting commit message.

--- c++/873_LengthOfLongestFibonacciSubsequence.cpp
+++ c++/873_LengthOfLongestFibonacciSubsequence.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& A) {
     	// return func1(A);
-    	return func2(A);
+    	// return func2(A);
+    	return func3(A);
     }
 
     // ** recursion top-down
@@ -32,7 +33,46 @@ public:
     }
 
     // ** recursion top-down + memo
+    // ** can't figure out, I give it up 0.0
     int func2(vector<int>& A) {
+    	return 0;
     }
 
+    // ** LCS between origin input and fibonacii sequence
+    // ** Totally wrong, cause [1, 3, 5] is not fibonacii-like,
+    // ** but it is a subsequence of [1,2,3,5,8...].
+    // ** Formula should be: subsequence of origin array, 
+    // ** but substring of fabonacii arry.
+    int func3(vector<int>& A) {
+    	if (A.size() <= 2) {
+    		return 0;
+    	}
+    	// ** init fabonacii sequence
+    	vector<int> fabo;
+    	fabo.push_back(1);
+    	fabo.push_back(2);
+    	while (fabo.back() <= A.back()) {
+    		int i = fabo.size() - 1;
+    		fabo.push_back(fabo[i-1] + fabo[i]);
+    	}
+
+    	// ** get LCS
+    	int maxLen = 0;
+    	vector<int> dp(fabo.size() + 1, 0);
+    	for (int i = 1; i <= A.size(); i++) {
+    		int pre = 0;
+    		for (int j = 1; j <= fabo.size(); j++) {
+    			int aux = dp[j];
+    			if (A[i-1] == fabo[j-1]) {
+    				dp[j] = pre + 1;
+    			} else {
+    				dp[j] = max(dp[j-1], dp[j]);
+    			}
+    			pre = aux;
+    			if (dp[j] > maxLen) maxLen = dp[j];
+    		}
+    	}
+
+    	return maxLen;
+    }
 };
