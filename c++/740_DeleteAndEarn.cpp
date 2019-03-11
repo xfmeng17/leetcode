9668ec427d7a63665a6cfc683fe782fe4b107b3d
xfmeng17 <xfmeng17@foxmail.com>
2019-03-11 18:48:44 +0800

    feat(740): 2nd, recursive+memoization, TLE
    Also O(n^3) time complexity, I think I need to find out
    a O(n^2) approach.

--- c++/740_DeleteAndEarn.cpp
+++ c++/740_DeleteAndEarn.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        return func1(nums);
+        // return func1(nums);
+        return func2(nums);
     }
 
     // ** top-down recursive, TLE
@@ -17,9 +18,9 @@ public:
                 singleSize.push_back(1);
             }
         }
-        return helper1(singleNums, 0, singleNums.size()-1, singleSize);
+        return helper1(singleNums, singleSize, 0, singleNums.size()-1);
     }
-    int helper1(vector<int>& singleNums, int lo, int hi, vector<int>& singleSize) {
+    int helper1(vector<int>& singleNums, vector<int>& singleSize, int lo, int hi) {
         if (lo < 0 || hi >= singleNums.size() || lo > hi) {
             return 0;
         }
@@ -28,17 +29,17 @@ public:
             int lf = 0;
             if (i-1 >= 0) {
                 if (singleNums[i-1] == singleNums[i]-1) {
-                    lf = helper1(singleNums, lo, i-2, singleSize);
+                    lf = helper1(singleNums, singleSize, lo, i-2);
                 } else {
-                    lf = helper1(singleNums, lo, i-1, singleSize);
+                    lf = helper1(singleNums, singleSize, lo, i-1);
                 }
             }
             int rt = 0;
             if (i+1 <= hi) {
                 if (singleNums[i+1] == singleNums[i]+1) {
-                    rt = helper1(singleNums, i+2, hi, singleSize);
+                    rt = helper1(singleNums, singleSize, i+2, hi);
                 } else {
-                    rt = helper1(singleNums, i+1, hi, singleSize);
+                    rt = helper1(singleNums, singleSize, i+1, hi);
                 }
             }
             int cur = lf + rt + singleNums[i] * singleSize[i];
@@ -46,5 +47,57 @@ public:
         }
 
         return ret;
+    }
+    
+    // ** recursive + memoization, still TLE
+    int func2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> singleNums;
+        vector<int> singleSize;
+        for (auto n : nums) {
+            if (singleNums.size() > 0 && singleNums.back() == n) {
+                singleSize.back()++;
+            } else {
+                singleNums.push_back(n);
+                singleSize.push_back(1);
+            }
+        }
+        
+        int n = singleNums.size();
+        vector<vector<int>> memo(n, vector<int>(n, 0));
+        
+        return helper2(singleNums, singleSize, 0, n-1, memo);
+    }
+    int helper2(vector<int>& singleNums, vector<int>& singleSize,
+        int lo, int hi, vector<vector<int>>& memo) {
+
+        if (lo < 0 || hi >= singleNums.size() || lo > hi) {
+            return 0;
+        }
+        if (memo[lo][hi] > 0) {
+            return memo[lo][hi];
+        }
+
+        for (int i = lo; i <= hi; i++) {
+            int lf = 0;
+            if (i-1 >= 0) {
+                if (singleNums[i-1] == singleNums[i]-1) {
+                    lf = helper2(singleNums, singleSize, lo, i-2, memo);
+                } else {
+                    lf = helper2(singleNums, singleSize, lo, i-1, memo);
+                }
+            }
+            int rt = 0;
+            if (i+1 <= hi) {
+                if (singleNums[i+1] == singleNums[i]+1) {
+                    rt = helper2(singleNums, singleSize, i+2, hi, memo);
+                } else {
+                    rt = helper2(singleNums, singleSize, i+1, hi, memo);
+                }
+            }
+            memo[lo][hi] = max(memo[lo][hi], lf + rt + singleNums[i] * singleSize[i]);
+        }
+
+        return memo[lo][hi];
     }
 };
