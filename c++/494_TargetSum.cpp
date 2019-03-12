1afa8d199ed1f8b167f698f7b7e3fac38df8a237
xfmeng17 <xfmeng17@foxmail.com>
2019-03-12 15:43:38 +0800

    feat(494): 1st, depth-first search, AC
    AC, but slow, how use dp to solve such problem?

---
+++ c++/494_TargetSum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        return func1(nums, S);      
+    }
+    
+    int func1(vector<int>& nums, int S) {
+        return helper1(nums, 0, S, 0);       
+    }
+
+    int helper1(vector<int>& nums, int i, int S, int sum) {
+        if (i >= nums.size()) {
+            return 0;
+        }
+
+        int plus = 0;
+        int minus = 0;
+        if (i == nums.size() - 1) {
+            if (sum + nums[i] == S) plus = 1;
+            if (sum - nums[i] == S) minus = 1;
+        } else {
+            plus = helper1(nums, i+1, S, sum + nums[i]);
+            minus = helper1(nums, i+1, S, sum - nums[i]);
+        }
+
+        return plus + minus;
+    }
+};
