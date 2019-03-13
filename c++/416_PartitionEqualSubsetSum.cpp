3ddb08ab45024f37d6cc8233f3191dd6ec652dbf
xfmeng17 <xfmeng17@foxmail.com>
2019-03-13 15:49:06 +0800

    feat(416): 2nd, 0/1 knapsack, 2 dimensional dp, AC
    Recall 0/1 knapscak problem, find out I'm totally forget
    this typical solution.
    
    With practicing too many dp problems, I get a little disgusted,
    but still need more practicing on such "subset sum" topic.

--- c++/416_PartitionEqualSubsetSum.cpp
+++ c++/416_PartitionEqualSubsetSum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        return func1(nums);     
+        // return func1(nums);
+        return func2(nums);     
     }
     // ** recursive
     bool func1(vector<int>& nums) {
@@ -30,4 +31,61 @@ public:
 
         return pick || notp;
     }
+
+    // ** 0/1 knapsack problem
+    bool func2(vector<int>& nums) {
+        int sum = 0;
+        for (auto n : nums) {
+            sum += n;
+        }
+        if (sum & 1) {
+            return false;
+        }
+        sum /= 2;
+        int n = nums.size();
+
+        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for (int j = 1; j <= sum; j++) {
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                dp[i][j] = dp[i-1][j];  
+                if (j - nums[i-1] >= 0) {
+                    dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+
+    // ** use 1-dimension
+    bool func3(vector<int>& nums) {
+        int sum = 0;
+        for (auto n : nums) {
+            sum += n;
+        }
+        if (sum & 1) {
+            return false;
+        }
+        sum /= 2;
+        int n = nums.size();
+
+        vector<bool> dp(sum+1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - nums[i-1] >= 0) {
+                    dp[j] = dp[j] || dp[j - nums[i-1]];
+                }
+            }
+        }
+
 };
