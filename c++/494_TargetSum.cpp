465f76a69dc9870c592a2f5347869fe71083d1c6
xfmeng17 <xfmeng17@foxmail.com>
2019-03-14 20:12:36 +0800

    feat(494): 2nd, 2D 0/1 knapsack, MLE
    Learn a day of 0/1 knapsack problem, find out that is vary
    important to understand how convert 2D dp to 1D.
    
    Get in touch with an intersting inplement about
    "complete 0/1 knapsack" problem in which the number of
    items is unlimited.
    
    Today, I make some progress!

--- c++/494_TargetSum.cpp
+++ c++/494_TargetSum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
-        return func1(nums, S);      
+        // return func1(nums, S);
+        return func2(nums, S);  
     }
     
     int func1(vector<int>& nums, int S) {
@@ -23,5 +24,39 @@ public:
         }
 
         return plus + minus;
-    }    
+    }
+
+    // ** dp from discuss, two-dimensional
+    // ** Memory Limit Exceeded
+    int func2(vector<int>& nums, int S) {
+        int sum = 0;
+        for (auto n : nums) {
+            sum += n;
+        }
+        sum += S;
+
+        if (sum & 1) {
+            return 0;
+        }
+
+        int plus = sum / 2;
+
+        vector<vector<int>> dp(nums.size()+1, vector<int>(plus+1, 0));
+        for (int i = 0; i <= nums.size(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= nums.size(); i++) {
+            for (int j = plus; j - nums[i-1] >= 0; j--) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
+            }
+        }
+
+        // for (int i = 0; i <= nums.size(); i++) {
+        //     for (int j = 0; j <= plus; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[nums.size()][plus];
+    }
 };
