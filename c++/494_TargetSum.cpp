d6d6be1047bacf1b80a4bb3076f9f68cf217db46
xfmeng17 <xfmeng17@foxmail.com>
2019-03-15 15:25:02 +0800

    style(494): add some annotation in 2D implement
    2D implement help me understand 1D implement.

--- c++/494_TargetSum.cpp
+++ c++/494_TargetSum.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
         // return func1(nums, S);
-        // return func2(nums, S);
-        return func3(nums, S);
+        return func2(nums, S);
+        // return func3(nums, S);
     }
     
     int func1(vector<int>& nums, int S) {
@@ -30,39 +30,55 @@ public:
     // ** dp from discuss, two-dimensional
     // ** Memory Limit Exceeded
     int func2(vector<int>& nums, int S) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum < S) { return 0; }
-        if ((sum + S) & 1) { return 0; }
+        int all = accumulate(nums.begin(), nums.end(), 0);
+        if (all < S) { return 0; }
+        if ((all + S) & 1) { return 0; }
 
-        int target = (sum + S) / 2;
-        vector<vector<int>> dp(nums.size()+1, vector<int>(target+1, 0));
+        int sum = (all + S) / 2;
+        vector<vector<int>> dp(nums.size()+1, vector<int>(sum+1, 0));
+
+        // ** When sum = 0:
+        // ** dp[0][0] means we have nothing, and our goal is 0, just can pick noting => 1 choice,
+        // ** dp[1][0] means we have { nums[0] }, and our goal is 0, just can noting => 1 choice,
+        // ** dp[2][0] means we have { nums[0], nums[1] }, and our goal is 0, just can pick noting => 1 choice,
+        // ** and so on...
         for (int i = 0; i <= nums.size(); i++) {
             dp[i][0] = 1;
         }
+
         for (int i = 1; i <= nums.size(); i++) {
-            // for (int j = 0; j <= target; j++) {
+            // ** down-top, more readable in this problem
+
+            // for (int j = 0; j <= sum; j++) {
             //     dp[i][j] = dp[i-1][j];
             //     if (j >= nums[i-1]) {
             //         dp[i][j] += dp[i-1][j - nums[i-1]];
             //     }
             // }
-            int j = target;
+
+            // ** top-down
+
+            int j = sum;
             for (; j >= nums[i-1]; j--) {
                 dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
             }
+            // ** We need to set dp[i][j] = dp[i-1][j] when current sum j
+            // ** is too small to let us 'pick' a nums[i-1].
+            // ** This leaves us just one condition that we get the sum j by just
+            // ** repeating all ways to  get sum j through the first i-1 elements. 
             for (; j >= 0; j--) {
                 dp[i][j] = dp[i-1][j];
             }
         }
 
         // for (int i = 0; i <= nums.size(); i++) {
-        //     for (int j = 0; j <= target; j++) {
+        //     for (int j = 0; j <= sum; j++) {
         //         cout << dp[i][j] << " ";
         //     }
         //     cout << endl;
         // }
 
-        return dp[nums.size()][target];
+        return dp[nums.size()][sum];
     }
 
     // ** dp from discuss, one-dimensional
