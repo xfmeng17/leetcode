2e52cb72589e765fc8f08b41548883a25016cec8
xfmeng17 <xfmeng17@foxmail.com>
2019-03-15 11:44:28 +0800

    feat(494): 3rd, 2D 0/1 knapsack, AC
    Use 2D dp array to solve this prolbem and then optimizing 2D to 1D
    array is very helpful in enhancing understanding 0/1 knapsack
    problem.
    
    Good job, maybe I should write this explanation in leetcode to
    help ohters understand 2D approach.

--- c++/494_TargetSum.cpp
+++ c++/494_TargetSum.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
         // return func1(nums, S);
-        return func2(nums, S);  
+        // return func2(nums, S);
+        return func3(nums, S);
     }
     
     int func1(vector<int>& nums, int S) {
@@ -29,34 +30,56 @@ public:
     // ** dp from discuss, two-dimensional
     // ** Memory Limit Exceeded
     int func2(vector<int>& nums, int S) {
-        int sum = 0;
-        for (auto n : nums) {
-            sum += n;
-        }
-        sum += S;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < S) { return 0; }
+        if ((sum + S) & 1) { return 0; }
 
-        if (sum & 1) {
-            return 0;
-        }
-
-        int plus = sum / 2;
-
-        vector<vector<int>> dp(nums.size()+1, vector<int>(plus+1, 0));
+        int target = (sum + S) / 2;
+        vector<vector<int>> dp(nums.size()+1, vector<int>(target+1, 0));
         for (int i = 0; i <= nums.size(); i++) {
             dp[i][0] = 1;
         }
         for (int i = 1; i <= nums.size(); i++) {
-            for (int j = plus; j - nums[i-1] >= 0; j--) {
+            // for (int j = 0; j <= target; j++) {
+            //     dp[i][j] = dp[i-1][j];
+            //     if (j >= nums[i-1]) {
+            //         dp[i][j] += dp[i-1][j - nums[i-1]];
+            //     }
+            // }
+            int j = target;
+            for (; j >= nums[i-1]; j--) {
                 dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
+            }
+            for (; j >= 0; j--) {
+                dp[i][j] = dp[i-1][j];
             }
         }
 
         // for (int i = 0; i <= nums.size(); i++) {
-        //     for (int j = 0; j <= plus; j++) {
+        //     for (int j = 0; j <= target; j++) {
         //         cout << dp[i][j] << " ";
         //     }
         //     cout << endl;
         // }
-        return dp[nums.size()][plus];
+
+        return dp[nums.size()][target];
+    }
+
+    // ** dp from discuss, one-dimensional
+    int func3(vector<int>& nums, int S) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < S) { return 0; }
+        if ((sum + S) & 1) { return 0;}
+
+        int target = (sum + S) >> 1;
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for (auto n : nums) {
+            for (int j = target; j >= n; j--) {
+                dp[j] += dp[j-n];
+            }
+        }
+
+        return dp[target];
     }
 };
