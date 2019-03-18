938617630bd6a2b55e0abc546e74da70810b95a1
xfmeng17 <xfmeng17@foxmail.com>
2019-03-18 17:16:16 +0800

    feat(698): 2nd, backtracking, time O(K^N), TLE
    Backtracking is so so slow..

--- c++/698_PartitionToKEqualSumSubsets.cpp
+++ c++/698_PartitionToKEqualSumSubsets.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        return func1(nums, k);
+        // return func1(nums, k);
+        return func2(nums, k);
     }
 
     // ** WA, case [10,10,10,7,7,7,6,6,6]
@@ -33,5 +34,42 @@ public:
         }
 
         return res;
+    }
+
+    // ** backtracking
+    // ** time O(k^N), N = nums.size(), TLE
+    bool func2(vector<int>& nums, int k) {
+        int all = 0;
+        for (auto n : nums) {
+            all += n;
+        }
+        if (all % k != 0) {
+            return false;
+        }
+        int sum = all / k;
+        vector<int> memo(k, 0);
+
+        return helper2(nums, 0, memo, sum);
+    }
+    bool helper2(vector<int>& nums, int idx, vector<int>& memo, int sum) {
+        if (idx >= nums.size()) {
+            for (auto m : memo) {
+                if (m != sum) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        for (int i = 0; i < memo.size(); i++) {
+            memo[i] += nums[idx];
+            if (helper2(nums, idx+1, memo, sum)) {
+                return true;
+            } else {
+                memo[i] -= nums[idx];
+            }
+        }
+
+        return false;
     }
 };
