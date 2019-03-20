9b24f95aa784ba0b82490b2026a332340abb34bb
xfmeng17 <xfmeng17@foxmail.com>
2019-03-20 16:29:33 +0800

    feat(376): 2nd, reference dp, AC
    Brilliant idea with using two array up[] and down[] to record the
    max wiggle subsequence length so far at current index.
    
    I think I should classify this problem into the topic that
    "Problems-Should-Practice-Repeatedly".

--- c++/376_WiggleSubsequence.cpp
+++ c++/376_WiggleSubsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        return func1(nums);
+        // return func1(nums);
+        return func2(nums);
     }
 
     // ** 1D array dp
-    // ** dp1[k] represents the max wiggle subsequence
-    // ** length that end at nums[k] and the last difference
-    // ** is positive, dp2[k] is same but last difference
-    // ** is negative.
+    // ** dp1[k] represents the max length that end at
+    // ** nums[k] and the last difference is positive,
+    // ** dp2[k] is same but last difference is negative.
     int func1(vector<int>& nums) {
         int N = nums.size();
         if (N <= 0) {
@@ -25,7 +25,7 @@ public:
             for (int j = i-1; j >= 0; j--) {
                 if (nums[i] > nums[j]) {
                     dp1[i] = max(dp1[i], 1 + dp2[j]);
-                } else  if (nums[i] < nums[j]) {
+                } else if (nums[i] < nums[j]) {
                     dp2[i] = max(dp2[i], 1 + dp1[j]);
                 }
                 res = max(res, max(dp1[i], dp2[i]));
@@ -33,5 +33,36 @@ public:
         }
 
         return res;
+    }
+
+    // ** Reference form discuss
+    // ** Brilliant idea, use two array up[] and down[]
+    // ** to record the max length so far at index i
+    int func2(vector<int>& nums) {
+        int N = nums.size();
+        if (N <= 0) {
+            return 0;
+        }
+
+        vector<int> up(N, 0);
+        vector<int> down(N, 0);
+
+        up[0] = 1;
+        down[0] = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (nums[i] > nums[i-1]) {
+                up[i] = down[i-1] + 1;
+                down[i] = down[i-1];
+            } else if (nums[i] < nums[i-1]) {
+                up[i] = up[i-1];
+                down[i] = up[i-1] + 1;
+            } else {
+                up[i] = up[i-1];
+                down[i] = down[i-1];
+            }
+        }
+
+        return max(up[N-1], down[N-1]);
     }
 };
