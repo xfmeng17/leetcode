b324c308fd3328c94549c9fc98c758bd7c3471c6
xfmeng17 <xfmeng17@foxmail.com>
2019-03-21 16:15:03 +0800

    feat(139): 2nd, 1D array dp, AC
    Change dp array meaning, use 1D array dp[i] to store s[0, i] is
    in-table or not.
    
    Practice makes me find the way out cause I have encounted similar
    problem before.

--- c++/139_WordBreak.cpp
+++ c++/139_WordBreak.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        return func1(s, wordDict);
+        // return func1(s, wordDict);
+        return func2(s, wordDict);
     }
 
     // ** 2D array dp
-    // ** dp[i][j] represents s[i, j] is in table or not
+    // ** dp[i][j] stores s[i, j] is in-table or not
     // ** time O(N^3), AC but not fast enough
     bool func1(string s, vector<string>& wordDict) {
         int N = s.length();
@@ -37,5 +38,33 @@ public:
         }
 
         return dp[0][N-1];
+    }
+
+    // ** 1D array dp
+    // ** dp[i] stores s[0, i] is in-table or not
+    // ** time O(N^2) if ignore substr(), AC really fast
+    bool func2(string s, vector<string>& wordDict) {
+        int N = s.length();
+        if (N <= 0) {
+            return true;
+        }
+
+        vector<bool> dp(N, false);
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+        for (int i = 0; i < N; i++) {
+            if (dict.count(s.substr(0, i+1)) > 0) {
+                dp[i] = true;
+                continue;
+            }
+            for (int j = i; j > 0; j--) {
+                if (dp[j-1] && dict.count(s.substr(j, i-j+1)) > 0) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[N-1];
     }
 };
