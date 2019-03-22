68d642680643fea8d98d81e67dd5f9fb13c72de1
xfmeng17 <xfmeng17@foxmail.com>
2019-03-22 19:32:00 +0800

    feat(377): 2nd, 1D array dp, AC
    O(N^2) time, pay attention on signed integer overflow problem!
    
    How to solve it? How to don't store irrelevant result.

--- c++/377_CombinationSumIV.cpp
+++ c++/377_CombinationSumIV.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        return func1(nums, target);
+        // return func1(nums, target);
+        return func2(nums, target);
     }
 
     // ** iterative
@@ -21,5 +22,41 @@ public:
             res += helper1(nums, sum + nums[i], target);
         }
         return res;
+    }
+
+    // ** 1D array dp
+    int func2(vector<int>& nums, int target) {
+        // vector<int> dp(target+1, 0);
+        vector<unsigned int> dp(target+1, 0);
+        for (auto n : nums) {
+            if (n <= target) {
+                dp[n] = 1;
+            }
+        }
+        for (int i = 1; i <= target; i++) {
+            for (auto n : nums) {
+                if (i - n > 0) {
+                    dp[i] += dp[i-n];
+                }
+            }
+        }
+        for (int i = 0; i <= target; i++) {
+            if (dp[i] > 1000000) {
+                cout<<"i="<<i<<", v="<<dp[i]<<endl;
+            }
+            
+        }
+        return dp[target];
+
+        // vector<int> dp(target+1, 0);
+        // dp[0] = 1;
+        // for (int i = 1; i <= target; i++) {
+        //     for (int j = 0; j < nums.size(); j++) {
+        //         if (i >= nums[j]) {
+        //             dp[i] += dp[i-nums[j]];
+        //         }
+        //     }
+        // }
+        // return dp[target];
     }
 };
