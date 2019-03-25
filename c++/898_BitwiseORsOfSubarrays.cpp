99bb7078382b5c988d8166b04fd73ea215ea8fc0
xfmeng17 <xfmeng17@foxmail.com>
2019-03-25 17:14:01 +0800

    feat(898): 2nd, reference with reducing iteration number, AC
    AC with runtime 1520 ms (fast than 48%) and memory usage 300MB
    (less than 45%). It's this solution good enough?

--- c++/898_BitwiseORsOfSubarrays.cpp
+++ c++/898_BitwiseORsOfSubarrays.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int subarrayBitwiseORs(vector<int>& A) {
-    	return func1(A);
+    	// return func1(A);
+    	// return func2(A);
+    	return func3(A);
     }
 
     /* dp[i][j] stores result of A[i..j]
@@ -28,5 +30,42 @@ public:
     	}
 
     	return table.size();
-    } 
+    }
+
+    // ** more concise
+    int func2(vector<int>& A) {
+    	int N = A.size();
+    	unordered_set<int> table;
+
+    	for (int i = 0; i < N; i++) {
+    		int cur = A[i];
+    		for (int j = i; j < N; j++) {
+    			cur |= A[j];
+    			if (table.count(cur) <= 0) {
+    				table.insert(cur);
+    			}
+    		}
+    	}
+
+    	return table.size();
+    }
+ 	
+ 	// ** reference, reduce iteration operations
+ 	int func3(vector<int>& A) {
+ 		int N = A.size();
+ 		unordered_set<int> res;
+ 		unordered_set<int> pre;
+
+ 		for (int i = 0; i < N; i++) {
+ 			unordered_set<int> cur;
+ 			cur.insert(A[i]);
+ 			for (auto p : pre) {
+ 				cur.insert(p | A[i]);
+ 			}
+ 			pre = cur;
+ 			res.insert(pre.begin(), pre.end());
+ 		}
+
+ 		return res.size();
+ 	}
 };
