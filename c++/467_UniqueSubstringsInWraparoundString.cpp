bc766456d0ff2c2a7735c2d640366c7969dce8f7
xfmeng17 <xfmeng17@foxmail.com>
2019-03-25 11:06:19 +0800

    feat(467): 2nd, add vector to filter substrings, WA
    Can't solve circulation input like this:
    "a...za...za...za...z...", need a new dp solution!

--- c++/467_UniqueSubstringsInWraparoundString.cpp
+++ c++/467_UniqueSubstringsInWraparoundString.cpp
@@ -1,40 +1,66 @@
 class Solution {
 public:
-    int findSubstringInWraproundString(string p) {
-    	return func1(p);
-    }
+	int findSubstringInWraproundString(string p) {
+		return func1(p);
+	}
 
-    // ** wa, bad case: "zaba"
-    // ** I double count "a" but "zab" already count a
-    int func1(string p) {
-    	unordered_set<string> table;
+	// ** wa, bad case: "zaba"
+	// ** I double count "a" but "zab" already count a
 
-    	int lo = 0;
-    	int hi = 1;
-    	while (hi < p.length()) {
-    		int diff = (p[hi] + 26 - p[hi-1]) % 26;
-    		if (diff == 1) {
-    			hi++;
-    		} else {
-    			string s = p.substr(lo, hi - lo);
-    			if (table.count(s) <= 0) {
-    				table.insert(s);
-    			}
-    			lo = hi;
-    			hi++;
-    		}
-    	}
-    	string s = p.substr(lo, hi - lo);
+	/* 	Update190325 use iteration with a vector to filter
+		the substrings already exists.
+		But WA the same cause can't solve circulation like this:
+		"a..za...za...z"
+	*/
+	int func1(string p) {
+		unordered_set<string> table;
+		int lo = 0;
+		int hi = 1;
+		while (hi < p.length()) {
+			int diff = (p[hi] + 26 - p[hi-1]) % 26;
+			if (diff == 1) {
+				hi++;
+			} else {
+				string s = p.substr(lo, hi - lo);
+				if (table.count(s) <= 0) {
+					table.insert(s);
+				}
+				lo = hi;
+				hi++;
+			}
+		}
+		
+		string s = p.substr(lo, hi - lo);
 		if (table.count(s) <= 0) {
 			table.insert(s);
 		}
 
+		// int res = 0;
+		// for (auto it = table.begin(); it != table.end(); it++) {
+		// 	int len = (*it).length();
+		// 	res += (1 + len) * len / 2;
+		// }
+
+		vector<string> list(table.begin(), table.end());
+		vector<bool> find(table.size(), false);
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if (i == j) continue;
+				if (list[j].find(list[i]) != string::npos) {
+					find[i] = true;
+					break;
+				}
+			}
+		}
 		int res = 0;
-		for (auto it = table.begin(); it != table.end(); it++) {
-			int len = (*it).length();
-			res += (1 + len) * len / 2;
+		for (int i = 0; i < list.size(); i++) {
+			// cout << list[i] << ": " << find[i] << endl;
+			if (!find[i]) {
+				int len = list[i].length();
+				res += (1 + len) * len / 2;
+			}
 		}
 
 		return res;
-    }
+	}
 };
