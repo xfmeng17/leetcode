2a863968ac9d7f74fe1056adb93342d315899fc6
xfmeng17 <xfmeng17@foxmail.com>
2019-03-25 13:31:18 +0800

    feat(467): 3rd: store every sub into hashset, TLE
    Clearly O(N^2) substring finding process will cause TLE

--- c++/467_UniqueSubstringsInWraparoundString.cpp
+++ c++/467_UniqueSubstringsInWraparoundString.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 	int findSubstringInWraproundString(string p) {
-		return func1(p);
+		// return func1(p);
+		return func2(p);
 	}
 
 	// ** wa, bad case: "zaba"
@@ -62,5 +63,43 @@ public:
 		}
 
 		return res;
+	}
+
+	int func2(string p) {
+		if (p.length() <= 0) {
+			return 0;
+		}
+		
+		unordered_set<string> table;
+		int lo = 0;
+		int hi = 1;
+		while (hi < p.length()) {
+			int diff = (p[hi] + 26 - p[hi-1]) % 26;
+			if (diff == 1) {
+				hi++;
+			} else {
+				putSubstring(p, lo, hi-1, table);
+				lo = hi;
+				hi++;
+			}
+		}
+		putSubstring(p, lo, hi-1, table);
+
+		return table.size();
+		
+	}
+
+	void putSubstring(string p, int lo, int hi, unordered_set<string>& table) {
+		if (lo > hi) return;
+
+		for (int len = hi - lo + 1; len >= 1; len--) {
+			for (int i = lo, j = lo + len - 1; j <= hi; i++, j++) {
+				string s = p.substr(i, len);
+				if (table.count(s) <= 0) {
+					// cout << table.size() << " = " << s << endl;
+					table.insert(s);
+				}
+			}
+		}
 	}
 };
