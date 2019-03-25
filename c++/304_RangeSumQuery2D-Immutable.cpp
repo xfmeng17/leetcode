2921cbfdc54542a3d420e54db361f97c6f961ff6
xfmeng17 <xfmeng17@foxmail.com>
2019-03-25 18:44:55 +0800

    feat(304): 2nd, matrix math, AC
    sum[i][j] means the sum from point (i, j) to (0, 0).

--- c++/304_RangeSumQuery2D-Immutable.cpp
+++ c++/304_RangeSumQuery2D-Immutable.cpp
@@ -1,35 +1,45 @@
-class NumMatrix {
-
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
+
+class NumMatrix {
+
 public:
-	vector<vector<int>> mat;
-	unordered_map<string, int> res;
-	const char SP = '|';
+	vector<vector<int>> sum;
 
 public:
     NumMatrix(vector<vector<int>> matrix) {
-		mat = matrix;
-		res.clear();
+    	sum = matrix;
+    	int m = matrix.size();
+    	int n = m > 0 ? matrix[0].size() : 0;
+
+    	for (int i = 0; i < m; i++) {
+    		for (int j = 1; j < n; j++) {
+    			sum[i][j] += sum[i][j-1];
+    		}
+    	}
+    	for (int i = 1; i < m; i++) {
+    		for (int j = 0; j < n; j++) {
+    			sum[i][j] += sum[i-1][j];
+    		}
+    	}
+
+    	// for (int i = 0; i < m; i++) {
+    	// 	for (int j = 0; j < n; j++) {
+    	// 		cout << sum[i][j] << " ";
+    	// 	}
+    	// 	cout << endl;
+    	// }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-    	string s = to_string(row1) + SP + to_string(col1) + SP + to_string(row2) + SP + to_string(col2);
-    	auto it = res.find(s);
-    	if (it != res.end()) {
-    		return it->second;
-    	}
-
-    	int sum = 0;
-    	for (int i = row1; i <= row2; i++) {
-    		for (int j = col1; j <= col2; j++) {
-    			sum += mat[i][j];
-    		}
-    	}
-    	res[s] = sum;
-    	return sum;
+        int d3 = sum[row2][col2];
+        int d2 = col1 - 1 >= 0 ? sum[row2][col1-1] : 0;
+        int d1 = row1 - 1 >= 0 ? sum[row1-1][col2] : 0;
+        int d0 = row1 - 1 >= 0 && col1 - 1 >= 0 ? sum[row1-1][col1-1] : 0;
+        return d3 - d2 - d1 + d0;
     }
 };
+
