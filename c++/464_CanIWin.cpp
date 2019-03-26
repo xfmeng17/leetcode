e918fb6b29485e2533d806bcb9816b5e8eddc591
xfmeng17 <xfmeng17@foxmail.com>
2019-03-26 20:57:08 +0800

    feat(464): 2nd, recursive + memoization from discuss, AC
    Reference from https://leetcode.com/problems/can-i-win/discuss/95277 :
    
    Use memoization could reduce time to O(2^N) (Without memo, time
    complexity should be like O(N!).
    
    For this question, the key part is: "what is the state of the game"?
    Intuitively, to uniquely determine the result of any state, we need to
    konw:
        1. The unchoosen numbsers.
        2. The remaining desiredTotal to reach.
    
    A second thought reveals that 1) and 2) are actually related because we
    can always get the 2) by deducting the sum of chosen numbers from
    originial desiredTotal.
    
    Then the problem becomes how to describe the state using 1).
    
    ....
    
    For myself, the part not figure it out is state-2 is state-1 actually!
    That is new to me.

--- c++/464_CanIWin.cpp
+++ c++/464_CanIWin.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
-        return func1(maxChoosableInteger, desiredTotal);
+        // return func1(maxChoosableInteger, desiredTotal);
+        return func2(maxChoosableInteger, desiredTotal);
     }
 
     // ** backtracking
@@ -43,4 +44,58 @@ public:
 	    	return res;
     	}
     }
+
+
+
+    // ** Reference, recursive + memoization
+    // ** Kye point is how to cache subproblems
+    bool func2(int maxChoosableInteger, int desiredTotal) {
+    	int sum = (1 + maxChoosableInteger) * maxChoosableInteger / 2;
+    	if (sum < desiredTotal) return false;
+    	if (desiredTotal <= 0) return true;
+
+    	unordered_map<int, bool> memo;
+    	vector<bool> marked(maxChoosableInteger + 1, false);
+
+    	return helper2(desiredTotal, marked, memo);
+    }
+
+    bool helper2(int desiredTotal, vector<bool>& marked,
+    	unordered_map<int, bool>& memo) {
+
+    	if (desiredTotal <= 0) return false;
+
+    	int key = format(marked);
+    	if (memo.find(key) != memo.end()) {
+    		return memo[key];
+    	}
+
+    	for (int i = 1; i < marked.size(); i++) {
+    		if (marked[i]) continue;
+    		
+    		marked[i] = true;
+    		if (!helper2(desiredTotal - i, marked, memo)) {
+    			memo[key] = true;
+    			marked[i] = false;
+    			return true;
+    		}
+    		marked[i] = false;
+
+    	}
+
+    	memo[key] = false;
+    	return false;
+    }
+
+    int format(vector<bool>& marked) {
+    	int num = 0;
+    	for (int i = marked.size() - 1; i >= 1; i--) {
+    		if (marked[i]) {
+    			num |= 1;
+    		}
+    		num = num << 1;
+    	}
+    	return num;
+    }
+
 };
