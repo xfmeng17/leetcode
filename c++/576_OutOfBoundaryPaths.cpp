086e19551be2adcce2713bbda160ff0777e5374e
xfmeng17 <xfmeng17@foxmail.com>
2019-03-26 14:36:44 +0800

    feat(576): 3rd, bottom-up with two 2D array dp, AC
    Really fast and memory usage less than 100%.

--- c++/576_OutOfBoundaryPaths.cpp
+++ c++/576_OutOfBoundaryPaths.cpp
@@ -4,7 +4,8 @@ public:
 public:
     int findPaths(int m, int n, int N, int i, int j) {
     	// return func1(m, n, N, i, j);
-    	return func2(m, n, N, i, j);
+    	// return func2(m, n, N, i, j);
+    	return func3(m, n, N, i, j);
     }
 
     int func1(int m, int n, int N, int i, int j) {
@@ -64,5 +65,58 @@ public:
     	memo[N][i][j] %= MOD;
 
     	return memo[N][i][j];
+    }
+
+    // ** two 2D array bottom-up
+    int func3(int m, int n, int N, int i, int j) {
+    	vector<vector<int>> dp1(m, vector<int>(n, 0));
+    	vector<vector<int>> dp2(m, vector<int>(n, 0));
+
+    	// ** N = 0
+    	if (N <= 0) return 0;
+    	
+    	// ** N = 1 or more
+    	for (int k = 1; k <= N; k++) {
+    		for (int x = 0; x < m; x++) {
+    			for (int y = 0; y < n; y++) {
+    				dp1[x][y] = 0;
+    				// ** up
+    				if (x - 1 >= 0) {
+    					dp1[x][y] += dp2[x-1][y];
+    				} else {
+    					dp1[x][y] += 1;
+    				}
+    				// ** down
+    				if (x + 1 < m) {
+    					dp1[x][y] += dp2[x+1][y];
+    					dp1[x][y] %= MOD;
+    				} else {
+    					dp1[x][y] += 1;
+    				}
+    				// ** left
+    				if (y - 1 >= 0) {
+    					dp1[x][y] += dp2[x][y-1];
+    					dp1[x][y] %= MOD;
+    				} else {
+    					dp1[x][y] += 1;
+    				}
+    				// ** right
+    				if (y + 1 < n) {
+    					dp1[x][y] += dp2[x][y+1];
+    					dp1[x][y] %= MOD;
+    				} else {
+    					dp1[x][y] += 1;
+    				}
+    			}
+    		}
+    		// ** copy dp1 to dp2
+    		for (int x = 0; x < m; x++) {
+    			for (int y = 0; y < n; y++) {
+    				dp2[x][y] = dp1[x][y];
+    			}
+    		}
+    	}
+
+    	return dp1[i][j];
     }
 };
