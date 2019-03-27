d5aa79a9b39e512a2c756abef2b19dbd58b5ed56
xfmeng17 <xfmeng17@foxmail.com>
2019-03-27 17:54:58 +0800

    feat(91): 2nd, dp reduce space, AC
    Really fast with O(1) space.

--- c++/91_DecodeWays.cpp
+++ c++/91_DecodeWays.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int numDecodings(string s) {
-    	return func1(s);
+    	// return func1(s);
+    	return func2(s);
     }
 
     // ** dp[i] = dp[i-1] + { dp[i-2] if nums[i-1][i] < 26 }
@@ -28,5 +29,32 @@ public:
         }
 
         return dp[N];
+    }
+
+    int func2(string s) {
+    	int N = s.length();
+    	if (N <= 0) {
+    		return 0;
+    	}
+
+    	int dp0 = 1;
+    	int dp1 = s[0] != '0' ? 1 : 0;
+
+    	for (int i = 1; i < N; i++) {
+    		int dp2 = 0;
+    		if (s[i] != '0') {
+    			dp2 += dp1;
+    		}
+    		if (s[i-1] != '0') {
+    			int n = (s[i-1] - '0') * 10 + (s[i] - '0');
+    			if (n >= 1 && n <= 26) {
+        			dp2 += dp0;
+        		}
+    		}
+    		dp0 = dp1;
+    		dp1 = dp2;
+    	}
+
+    	return dp1;
     }
 };
