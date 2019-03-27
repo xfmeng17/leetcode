109d219ad78e46ebe8e9fbbb367c631dda4544d8
xfmeng17 <xfmeng17@foxmail.com>
2019-03-27 17:12:31 +0800

    feat(525): 2nd, math from discuss, AC
    Reference:
    https://leetcode.com/problems/continuous-subarray-sum/discuss/99499:
    
    We iterate through the input array exactly once, keeping track of the
    running sum mod k of the elements in the process. If we find that a
    running sum value at index j has been previously seen before in some
    earlier index i, then we know that the sub-array (i, j] contains a
    desired sum.

--- c++/525_ContinuousSubarraySum.cpp
+++ c++/525_ContinuousSubarraySum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-    	return func1(nums, k);
+    	// return func1(nums, k);
+    	return func2(nums, k);
     }
 
     // ** dp[i] = sum nums from 0 to i
@@ -35,4 +36,50 @@ public:
 
     	return false;
     }
+
+    /* Reference, math! find the mod that repeats
+	 *
+	 * If we find that a running sum value at index j
+	 * has been previously seen before in some earlier
+	 * index i, then we know the sub-array (i, j] contains
+	 * a desired sum.
+	 *
+	 * Pay attention the index of initial sum=0 is -1 
+	*/
+	
+	bool func2(vector<int>& nums, int k) {
+		int N = nums.size();
+		if (N <= 1) {
+			return false;
+		}
+
+		if (k == 0) {
+			for (int i = 0; i < N-1; i++) {
+				if (nums[i] == 0 && nums[i+1] == 0) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		unordered_map<int, int> map;
+
+		// ** the index of sum=0 is -1
+		map[0] = -1;
+
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += nums[i];
+			sum %= k;
+			if (map.count(sum) > 0) {
+				if (i - map[sum] >= 2) {
+					return true;
+				}
+			} else {
+				map[sum] = i;
+			}
+		}
+
+		return false;
+	} 
 };
