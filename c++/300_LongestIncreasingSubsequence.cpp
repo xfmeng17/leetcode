f2a494f7314d4137e6633b081aefde0b66b8aada
xfmeng17 <xfmeng17@foxmail.com>
2019-03-28 19:48:34 +0800

    feat(300): 2nd, O(NlogN) time from discuss, AC
    https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824:
    
    `tails` is an array storing the smallest tail of all increasing
    subsequences with length `i+1` in `tails[i]`.
    
    For example, say we have nums = [4,5,6,3], then all the avaiable
    increasing subsequences are:
    len = 1: [4], [5], [6], [3] => tails[1] = 3
    len = 2: [4,5], [5,6] => tails[2] = 5
    len = 3" [4,5,6] => tails[3] = 6
    
    We can easily prove that tails is a increasing array. Therefore it is
    possible to do a binary search in tails array to find the one needs
    update.
    
    Each time we only do one of the two:
    1. if num is larger than all tails, append it, increase the len by 1.
    2. if tails[i-1] < num < tails[i], update tails[i]
    
    Doing so will maintain the tails invariant. The final answer is just the
    len.
    
    ...
    
    This is a great solution! Take me a lot time to understand. Should
    practice this problem again and again.

--- c++/300_LongestIncreasingSubsequence.cpp
+++ c++/300_LongestIncreasingSubsequence.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-    	return func1(nums);
+    	// return func1(nums);
+    	return func2(nums);
     }
 
     // ** dp[i] represents the LIS end at i
     int func1(vector<int>& nums) {
     	int N = nums.size();
     	if (N <= 0) return 0;
-    	
+
     	vector<int> dp(N, 1);
 
   		int res = 1;
@@ -22,5 +23,54 @@ public:
     	}
 
     	return res;
+    }
+
+    // ** Reference, using a array called 'tails' to store
+    // ** the smallest tail with the len of LIS which
+    // ** may 1, 2, ..., N-1
+    int func2(vector<int>& nums) {
+    	int N = nums.size();
+    	vector<int> tails(N+1, -1);
+
+    	bool doPrint = false;
+    	
+    	int len = 1;
+    	for (auto num : nums) {
+
+            int lo = 1, hi = len;
+
+    		if (doPrint) {
+    			printf("beg: lo=%d, hi=%d\n", lo, hi);
+    		}
+
+    		while (lo < hi) {
+    			int mid = (lo + hi) / 2;
+    			if (tails[mid] < num) {
+    				lo = mid + 1; 
+    			} else {
+    				hi = mid;
+    			}
+    		}
+    		tails[lo] = num;
+
+    		if (doPrint) {
+    			printf("end: lo=%d, hi=%d\n", lo, hi);
+    		}
+
+    		if (lo == len) {
+    			len++;
+    		}
+
+    		if (doPrint) {
+    			for (int i = 0; i <= N; i++) {
+	    			printf("length = %d with tail = %d\n", i, tails[i]);
+	    		}
+	    		printf("==========\n");
+    		}
+    		
+    	}
+
+
+    	return len - 1;
     }
 };
