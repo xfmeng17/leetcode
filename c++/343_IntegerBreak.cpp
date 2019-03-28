9fdcfb39b5080acb8a38b7a27afd602cdc9de799
xfmeng17 <xfmeng17@foxmail.com>
2019-03-28 15:51:01 +0800

    feat(343): 1st, one 1D array dp, AC
    Fast solved and fast runtime. Good job xf!

---
+++ c++/343_IntegerBreak.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        return func1(n);
+    }
+    int func1(int n) {
+        vector<int> dp(n+1, 0);
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = max(dp[i], j * max(i-j, dp[i-j]));
+            }
+        }
+
+        return dp[n];
+    }
+};
