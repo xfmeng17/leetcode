6fd8e6a843b75c6f14af1e563b75b3ff70af4ab6
xfmeng17 <xfmeng17@foxmail.com>
2019-03-29 16:50:27 +0800

    feat(264): 2nd, brilliant dp solution from discuss, AC
    Use 3 indexes to store the next index to multiply with 2, 3 or 5.
    
    Could think as a three pointers solution. Solved this problem 2 years
    ago but still get stuck today.

--- c++/264_UglyNumber.cpp
+++ c++/264_UglyNumber.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        return func1(n);
+        // return func1(n);
+        return func2(n);
     }
 
     // ** hash set
@@ -29,4 +30,22 @@ public:
 
         return last;
     }
+
+    // ** Reference
+    int func2(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        vector<int> dp(n, 0);
+        dp[0] =1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = min(dp[i2]*2, min(dp[i3]*3, dp[i5]*5));
+            if (dp[i] == dp[i2] * 2) i2++;
+            if (dp[i] == dp[i3] * 3) i3++;
+            if (dp[i] == dp[i5] * 5) i5++;
+        }
+
+        return dp[n-1];
+    }
+
 };
