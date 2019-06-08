5da7864eeaa91cb24a3e790ddca4d8dffc306af6
xfmeng17 <xfmeng17@foxmail.com>
2019-06-08 14:18:00 +0800

    feat(1003): [stack] use stack in func2()
    Not hard problem but the description is quite confusing.

--- c++/1003_CheckIfWordIsValidAfterSubstitutions.cpp
+++ c++/1003_CheckIfWordIsValidAfterSubstitutions.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isValid(string S) {
-      return func1(S);  
+        // return func1(S);
+        return func2(S);
     }
-
+    // ** ugly if-else branch hell
     bool func1(string S) {
         int N = S.length();
         int i = 0;
@@ -47,5 +48,29 @@ public:
         }
 
         return true;
+    }
+    
+    // ** stack
+    bool func2(string S) {
+        stack<char> stk;
+        int i = 0;
+        for (auto s : S) {
+            if (s == 'c') {
+                if (stk.size() < 2) {
+                    return false;
+                } else {
+                    char b = stk.top();
+                    stk.pop();
+                    char a = stk.top();
+                    stk.pop();
+                    if (b != 'b' || a != 'a') {
+                        return false;
+                    }
+                }
+            } else {
+                stk.push(s);
+            }
+        }
+        return stk.empty();
     }
 };
