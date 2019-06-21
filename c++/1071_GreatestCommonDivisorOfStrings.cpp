d69a24496a0b0b43c3d03c7f37178023338b3344
xfmeng17 <xfmeng17@foxmail.com>
2019-06-21 11:57:12 +0800

    feat(1071): [string] easy string iteration compare

---
+++ c++/1071_GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return func1(str1, str2);
+    }
+
+    string func1(string str1, string str2) {
+    	string txt = str1.length() > str2.length() ? str1 : str2;
+    	string pat = str1.length() > str2.length() ? str2 : str1;
+    	
+    	int len = pat.length() + 1;
+    	while (--len > 0) {
+    		if (pat.length() % len != 0 || txt.length() % len != 0) {
+    			continue;
+    		}
+
+    		//** check pattern
+    		int patSectionNum = pat.length() / len;
+    		bool patDoCheck = true;
+    		for (int sec = 0; sec < patSectionNum && patDoCheck; sec++) {
+    			for (int i = 0; i < len; i++) {
+	    			if (pat[i] != pat[i + len * sec]) {
+	    				patDoCheck = false;
+	    				break;
+	    			}
+	    		}
+    		}
+    		if (!patDoCheck) {
+    			continue;
+    		}
+    		//** check text
+    		int txtSectionNum = txt.length() / len;
+    		bool txtDoCheck = true;
+    		for (int sec = 0; sec < txtSectionNum && txtDoCheck; sec++) {
+    			for (int i = 0; i < len; i++) {
+	    			if (pat[i] != txt[i + len * sec]) {
+	    				txtDoCheck = false;
+	    				break;
+	    			}
+	    		}
+    		}
+    		if (!txtDoCheck) {
+    			continue;
+    		}
+
+    		return pat.substr(0, len);
+    	}
+    	return "";
+    }
+};
