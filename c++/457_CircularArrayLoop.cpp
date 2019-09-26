8932d0863a872ba720017859419fdf43efe15380
xfmeng17 <xfmeng17@foxmail.com>
2019-09-26 16:45:13 +0800

    feat(457): [Array] add solotion func2 and func3 and reduce both time and space

--- c++/457_CircularArrayLoop.cpp
+++ c++/457_CircularArrayLoop.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool circularArrayLoop(vector<int>& nums) {
-        return func1(nums);
+        // return func1(nums);
+        // return func2(nums);
+        return func3(nums);
     }
 
-    // straight forward O(n^2)
+    // Straight forward time: O(n^2), space O(n)
     bool func1(vector<int>& nums) {
     	int n = nums.size();
     	vector<int> marked(n, -1);
@@ -34,6 +36,70 @@ public:
 	    		cnt++;
     		}
     		if (ret) return true;
+    	}
+    	return false;
+    }
+
+    // Use another vector count reduce time to O(n)
+    bool func2(vector<int>& nums) {
+    	int N = nums.size();
+    	vector<int> marked(N, -1);
+    	vector<int> count(N, 0);
+    	for (int i = 0; i < N; i++) {
+    		bool right = nums[i] > 0 ? true : false;
+    		int cnt = 1;
+    		
+    		marked[i] = i;
+    		count[i] = cnt;
+    		int next = i;
+    		while (true) {
+    			next = (next + nums[next]) % N;
+    			if (next < 0) {
+    				next += N;
+    			}
+    			if ((right && nums[next] < 0) ||
+	    			(!right && nums[next] > 0)) {
+	    			break;
+	    		}
+	    		if (marked[next] == i) {
+	    			if (cnt - count[next] > 1) {
+	    				return true;
+	    			}
+	    			break;
+	    		}
+	    		cnt++;
+	    		marked[next] = i;
+	    		count[next] = cnt;
+    		}
+    	}
+    	return false;
+    }
+
+    // Upgrate func2() to only use O(1) space
+    bool func3(vector<int>& nums) {
+    	const int MAX = 20000;
+    	int N = nums.size();
+    	for (int i = 0; i < N; i++) {
+    		bool positive = nums[i] > 0 ? true : false;
+    		int next = i;
+
+    		while (nums[next] < MAX) {
+    			int prev = next;
+    			next = (next + nums[next]) % N;
+    			next = next < 0 ? next + N : next;
+    			nums[prev] = MAX + i;
+                
+    			if ((positive && nums[next] < 0) ||
+    				(!positive && nums[next] > 0 && nums[next] < MAX)) {
+    				break;
+    			}
+    			if (nums[next] >= MAX) {
+    				if (prev != next && nums[next] == MAX + i) {
+    					return true;
+    				}
+    				break;
+    			}
+    		}
     	}
     	return false;
     }
