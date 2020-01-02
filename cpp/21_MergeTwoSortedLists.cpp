cea6c347f957336fce3061465dd7f2982b041f6a
xfmeng17 <xfmeng17@foxmail.com>
2020-01-02 14:01:16 +0800

    feat(cpp/21): [list] easy

---
+++ cpp/21_MergeTwoSortedLists.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+  		if (l1 == NULL) {
+  			return l2;
+  		}
+  		if (l2 == NULL) {
+  			return l1;
+  		}
+
+  		ListNode *head = NULL;
+  		ListNode *curr = NULL;
+
+  		if (l1->val < l2->val) {
+  			head = l1;
+  			curr = head;
+  			l1 = l1->next;
+  		} else {
+  			head = l2;
+  			curr = head;
+  			l2 = l2->next;
+  		}
+
+  		while (l1 != NULL && l2 != NULL) {
+  			if (l1->val < l2->val) {
+  				curr->next = l1;
+  				l1 = l1->next;
+  			} else {
+  				curr->next = l2;
+  				l2 = l2->next;
+  			}
+  			curr = curr->next;
+  		}
+  		if (l1 != NULL) {
+  			curr->next = l1;
+  		}
+  		if (l2 != NULL) {
+  			curr->next = l2;
+  		}
+  		return head;
+    }
+};
