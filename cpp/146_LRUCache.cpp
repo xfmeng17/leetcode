63e71baa23e5acadc62d165572cea2922ffcda7d
xfmeng17 <xfmeng17@foxmail.com>
2020-01-05 22:33:05 +0800

    feat(cpp/146): [design] LRU-cache, hashmap+doublelinkedlist

--- cpp/146_LRUCache.cpp
+++ cpp/146_LRUCache.cpp
@@ -1,10 +1,12 @@
 class DLinkedList {
 public:
+    int key;
     int val;
     DLinkedList* prev;
     DLinkedList* next;
     
-    DLinkedList(int _val) {
+    DLinkedList(int _key, int _val) {
+        key = _key;
         val = _val;
         prev = NULL;
         next = NULL;
@@ -21,8 +23,8 @@ public:
     LRUCache(int capacity) {
         cap = capacity;
         size = 0;
-        head = new DLinkedList(0);
-        tail = new DLinkedList(0);
+        head = new DLinkedList(0, 0);
+        tail = new DLinkedList(0, 0);
         head->next = tail;
         tail->prev = head;
     }
@@ -35,30 +37,37 @@ public:
         if (map.find(key) == map.end()) {
             return -1;
         }
+
         int val = map[key]->val;
         put(key, val);
+        Dump(head, tail);
+
         return val;
     }
     
-    void put(int key, int value) {
-        if (size < cap) {
-            map[key] = Add(head, value);
+    void put(int key, int val) {
+        if (map.find(key) != map.end()) {
+            Delete(map[key]);
+            map[key] = Add(head, key, val);
+            Dump(head, tail);
             return;
         }
 
-        if (map.find(key) != map.end()) {
-            Delete(map[key]);
-            map[key] = Add(head, value);
-        } else {
-            Eliminate(head, tail);
-            map[key] = Add(head, value);
+        if (size < cap) {
+            map[key] = Add(head, key, val);
+            Dump(head, tail);
+            return;
         }
 
+        int elimKey = Eliminate(head, tail);
+        map.erase(elimKey);
+        map[key] = Add(head, key, val);
+        Dump(head, tail);
         return;
     }
     
-    DLinkedList* Add(DLinkedList* head, int val) {
-        DLinkedList* node = new DLinkedList(val);
+    DLinkedList* Add(DLinkedList* head, int key, int val) {
+        DLinkedList* node = new DLinkedList(key, val);
         DLinkedList* first = head->next;
         first->prev = node;
         node->next = first;
@@ -76,17 +85,26 @@ public:
         delete node;
         return;
     }
-    void Eliminate(DLinkedList* head, DLinkedList* tail) {
+    int Eliminate(DLinkedList* head, DLinkedList* tail) {
         DLinkedList* curr = tail->prev;
         if (curr == head) {
-            return;
+            return 0;
         }
+        int key = curr->key;
         DLinkedList* prev = curr->prev;
         prev->next = tail;
         tail->prev = prev;
         size--;
         delete curr;
+        return key;
+    }
+    void Dump(DLinkedList* head, DLinkedList* tail) {
         return;
+        while (head->next != tail) {
+            printf("p=%p, key=%d, val=%d\n", head->next, head->next->key, head->next->val);
+            head = head->next;
+        }
+        printf("***size=%d***\n", size);
     }
 };
 
@@ -94,5 +112,5 @@ public:
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
- * obj->put(key,value);
+ * obj->put(key,val);
  */
