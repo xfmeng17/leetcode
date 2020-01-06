3c47621930c8f5de520ace6cc837be7de729ba9f
xfmeng17 <xfmeng17@foxmail.com>
2020-01-06 22:31:21 +0800

    feat(cpp/138): [linkedlist] O(1) space is interesting

--- cpp/138_CopyListwithRandomPointer.cpp
+++ cpp/138_CopyListwithRandomPointer.cpp
@@ -16,6 +16,10 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // return func1(head);
+        return func2(head);
+    }
+    Node* func1(Node* head) {
         if (head == NULL) {
             return NULL;
         }
@@ -48,6 +52,46 @@ public:
         while (curr1) {
             if (curr1->random) {
                 curr2->random = map2[map1[curr1->random]];
+            }
+            curr1 = curr1->next;
+            curr2 = curr2->next;
+        }
+
+        return head2;
+    }
+    Node* func2(Node* head) {
+        if (head == NULL) {
+            return NULL;
+        }
+
+        Node* curr = head;
+        while (curr != NULL) {
+            Node* next = curr->next;
+            Node* copy = new Node(curr->val);
+            copy->random = curr->random;
+            curr->next = copy;
+            copy->next = next;
+            curr = next;
+        }
+
+        curr = head;
+        while (curr != NULL) {
+            if (curr->next->random != NULL) {
+                curr->next->random = curr->next->random->next;
+            }
+            curr = curr->next->next;
+        }
+
+        Node* head1 = head;
+        Node* head2 = head->next;
+        Node* curr1 = head1;
+        Node* curr2 = head2;
+        while (curr1 != NULL) {
+            curr1->next = curr2->next;
+            if (curr2->next != NULL) {
+                curr2->next = curr2->next->next;
+            } else {
+                curr2->next = NULL;
             }
             curr1 = curr1->next;
             curr2 = curr2->next;
