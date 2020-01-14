5e47a086c620a818ec79facca3c208321f9b9b98
xfmeng17 <xfmeng17@foxmail.com>
2020-01-14 22:21:22 +0800

    chore(notes): update

--- golang/673_NumberofLongestIncreasingSubsequence.go
+++ golang/673_NumberofLongestIncreasingSubsequence.go
@@ -1,7 +1,9 @@
 func findNumberOfLIS(nums []int) int {
-    return func1(nums);
+	// return func1(nums);
+	return func2(nums);
 }
 
+//** iterative, from right to left
 func func1(nums []int) int {
 	n := len(nums);
 	length := make([]int, n);
@@ -25,6 +27,44 @@ func func1(nums []int) int {
 			}
 		}
 		// fmt.Printf("i=%d, length=%d, count=%d\n", i, length[i], count[i]);
+		if length[i] > maxLen {
+			maxLen = length[i];
+		}
+	}
+
+	ret := 0;
+	for i := 0; i < n; i++ {
+		if length[i] == maxLen {
+			ret += count[i];
+		}
+	}
+
+	return ret;
+}
+
+//** iterative, from left to right
+func func2(nums []int) int {
+	n := len(nums);
+	length := make([]int, n);
+	count := make([]int, n);
+
+	for i := 0; i < n; i++ {
+		length[i] = 1;
+		count[i] = 1;
+	}
+
+	maxLen := 1;
+	for i := 0; i < n; i++ {
+		for j := i-1; j >= 0; j-- {
+			if nums[i] > nums[j] {
+				if length[j]+1 > length[i] {
+					length[i] = length[j]+1;
+					count[i] = count[j]; 
+				} else if length[j]+1 == length[i] {
+					count[i] += count[j];
+				}
+			}
+		}
 		if length[i] > maxLen {
 			maxLen = length[i];
 		}
