8d89858a48377865f7dd4db2de63a4bc0f439c24
xfmeng17 <xfmeng17@foxmail.com>
2020-01-24 15:09:22 +0800

    feat(cpp/0518): [kp] complete-kp

--- cpp/0322.cpp
+++ cpp/0322.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
   int coinChange(vector<int> &coins, int amount) {
-    return func1(coins, amount);
+    // return func1(coins, amount);
+    return func2(coins, amount);
   }
 
   // ** standard completely 0/1 knapsack
@@ -17,5 +18,20 @@ public:
       }
     }
     return dp[amount] < amount + 1 ? dp[amount] : -1;
+  }
+
+  // Update at 2020-01-24 use same code style
+  // Same problem to 528
+  int func1(vector<int> &coins, int amount) {
+    vector<int> kp(amount+1, amount+1);
+    kp[0] = 0;
+
+    for (int i = 1; i <= coins.size(); i++) {
+      for (int v = coins[i-1]; v <= amount; v++) {
+        kp[v] = min(kp[v], 1 + kp[v-coins[i-1]]);
+      }
+    }
+
+    return kp[amount] < amount+1 ? kp[amount] : -1;
   }
 };
