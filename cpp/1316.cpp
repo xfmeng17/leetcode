c33980da86e4b18da47f45c48faf0e663f8149de
xfmeng17 <xfmeng17@foxmail.com>
2020-01-25 18:50:53 +0800

    feat(cpp/1316): [string] unfinish Rabin-Karp solution

--- cpp/1316.cpp
+++ cpp/1316.cpp
@@ -2,63 +2,21 @@ class Solution {
 public:
     int distinctEchoSubstrings(string text) {
         // return func1(text);
-        // return func2(text);
-        return func3(text);
+        return func2(text);
     }
 
-    // TLE
-    int fun1(string text) {
+    // 1. Basic string compare solution, use hash-set do cache
+    // Time: O(n^3) time, Space: O(n^2)
+    int func1(string text) {
         int N = text.length();
-        unordered_map<string, int> map;
-        for (int len = 2; len <= N; len += 2) {
-            for (int lo = 0; lo < N; lo++) {
-                if (lo + len > N) { 
-                    break; 
-                }
-                string sub = text.substr(lo, len);
-                if (map.find(sub) != map.end()) {
-                    continue;
-                }
-                
-                bool put = true;
-                for (int j = 0; j < len/2; j++) {
-                    if (sub[j] != sub[j+len/2]) {
-                        put = false;
-                        break;
-                    }
-                }
-                if (put) {
-                    map[sub] = 1;
-                } else {
-                    map[sub] = 0;
-                }
-            }
-        }
-        int ret = 0;
-        for (auto it = map.begin(); it != map.end(); it++) {
-            if (it->second > 0) {
-                // cout << it->first << endl;
-                ret++;
-            }
-        }
-        return ret;
-    }
-    // sometime AC, sometime TLE, slow
-    int func2(string text) {
-        int N = text.length();
-        unordered_map<string, int> map;
-        
-        if (N <= 0) {
-            return 0;
-        }
+        unordered_set<string> set;
 
-        for (int len = N/2; len >= 1; len--) {
+        for (int len = 1; len <= N/2; len++) {
             for (int i = 0; i+2*len-1 < N; i++) {
                 string s = text.substr(i, len);
-                if (map.find(s) != map.end()) {
+                if (set.count(s) > 0) {
                     continue;
                 }
-
                 bool same = true;
                 for (int j = 0; j < len; j++) {
                     if (text[i+j] != text[i+j+len]) {
@@ -67,43 +25,77 @@ public:
                     }
                 }
                 if (same) {
-                    map[s]++;
-                }
-            }
-        }
-
-        return map.size();
-    }
-    // TLE also
-    int func3(string text) {
-        int N = text.length();
-        if (N <= 0) {
-            return 0;
-        }
-        vector<vector<bool>> result(N, vector<bool>(N, false));
-        
-        for (int len = 1; len <= N/2; len++) {
-            for (int i = 0; i+2*len-1 < N; i++) {
-                bool same = true;
-                for (int j = 0; j < len; j++) {
-                    if (text[i+j] != text[i+j+len]) {
-                        same = false;
-                        break;
-                    }
-                }
-                result[i][i+2*len-1] = same;
-            }
-        }
-        unordered_set<string> set;
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                if (result[i][j]) {
-                    set.emplace(text.substr(i, j-i+1));
+                    set.emplace(s);
                 }
             }
         }
         return set.size();
     }
 
-    // MUST use hashing to compare string
+    // 2. Rabin-Karp substring search
+    // According to "Algorithms, 4th Edition => 5.3 Substring Search => Rabin-Karp fingerprint search"
+    // Time: O(n^2), Space: O(1)
+    // Onile implement: https://algs4.cs.princeton.edu/53substring/  RabinKarp.java
+    
+    int func2(string text) {
+        const int R = 26;
+        const long Q = 1e9 + 7;
+
+        int N = text.length();
+        unordered_set<string> set;
+        for (int len = 1; len <= N/2; len++) {
+            int M = len;
+            long RM = RK_RM(M, R, Q);
+            long curr_hash1 = 0;
+            long curr_hash2 = 0;
+            // M == len, use 'M' to keep same to algs4's implement
+            for (int i = 0; i+2*len-1 < N; i++) {
+                if (i == 0) {
+                    curr_hash1 = RK_Hash(text, i, M, R, Q);
+                    curr_hash2 = RK_Hash(text, i+M, M, R, Q);
+                } else {
+                    curr_hash1 = RK_Shift(text, i, M, R, Q, RM, curr_hash1);
+                    curr_hash2 = RK_Shift(text, i+M, M, R, Q, RM, curr_hash2);
+                }
+                printf("i=%d, curr_hash1=%ld, curr_hash2=%ld\n", i, curr_hash1, curr_hash2);
+                if (curr_hash1 == curr_hash2 && RK_Recheck(text, i, i+M)) {
+                    set.emplace(text.substr(i, i+M));
+                }
+            }
+        }
+        return set.size();
+    }
+
+    long RK_RM(int M, int R, long Q) {
+        long RM = R;
+        int time = M-2;
+        while (time-- > 0) {
+            RM *= R;
+            RM %= Q;
+        }
+        return RM;
+    }
+    long RK_Hash(string text, int i, int M, int R, long Q) {
+        long hash = 0;
+        while (i++ < M) {
+            hash += (hash * R) + (text[i] - '0');
+            hash %= Q;
+        }
+        return hash;
+    }
+    long RK_Shift(string text, int i, int M, int R, long Q, long RM, long last_hash) {
+        int last_val = text[i-1] - '0';
+        int curr_val = text[i+M-1] - '0';
+        long curr_hash = (last_hash + last_val*(Q - RM)) * R + curr_val;
+        curr_hash %= Q;
+        return curr_hash;
+    }
+    bool RK_Recheck(string text, int i, int j) {
+        for (int len = 0; len < j - i; len++) {
+            if (text[i+len] != text[j+len]) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
