f13c10881671f833a57b988cbd9b00a8a5c884fa
xfmeng17 <xfmeng17@foxmail.com>
2020-01-25 14:51:09 +0800

    feat(biweekly16,17): summary

--- cpp/1301.cpp
+++ cpp/1301.cpp
@@ -1,4 +1,6 @@
 class Solution {
+private:
+    const int MOD = 1e9 + 7;
 public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
         return func1(board);
@@ -8,68 +10,80 @@ public:
         int M = board.size();
         int N = board[0].size();
 
-        vector<vector<int>> memo(M, vector<int>(N, 0));
-        vector<int> ret(2, 0);
-
-        helper1(board, M-1, N-1, memo, ret);
+        vector<int> ans(2, 0);
+        vector<vector<int>> memo(M, vector<int>(N, -1));
+        vector<vector<int>> nums(M, vector<int>(N, 0));
+        nums[0][0] = 1;
+        int ret = helper1(board, M-1, N-1, memo, nums);
         
+        if (memo[0][0] == -1) {
+            return ans;
+        }
+        ans[0] = ret;
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                printf("memo[%d][%d]=%d\n", i, j, memo[i][j]);
+                // printf("memo[%d][%d]=%d, nums=%d\n", i, j, memo[i][j], nums[i][j]);
+                if (memo[i][j] == ans[0] ) {
+                    ans[1] += nums[i][j];
+                }
             }
         }
-        if (memo[0][0] != -1) {
-            ret[0] = 0;
-            ret[1] = 0;
-        }
+        ans[0] -= 1;
 
-        return ret; 
+        return ans; 
     }
 
-    int helper1(vector<string>& board, int i, int j, vector<vector<int>>& memo, vector<int>& ret) {
-        if (i < 0 || j < 0) {
-            return 0;
-        }
+    int helper1(vector<string>& board, int i, int j,
+                vector<vector<int>>& memo, vector<vector<int>>& nums) {
         if (i == 0 && j == 0) {
+            memo[0][0] = 0;
             return 0;
         }
-        if (memo[i][j] > 0) {
+        if (memo[i][j] >= 0) {
             return memo[i][j];
         }
-        
-        int up = 0;
-        int up_left = 0;
-        int left = 0;
-
-        int curr = board[i][j] == 'S' ? 0 : board[i][j] - '0';
+  
+        int up = -1;
+        int up_left = -1;
+        int left = -1;
+        int curr = board[i][j] == 'S' ? 1 : board[i][j] - '0';
         if (i-1 >= 0 && board[i-1][j] != 'X') {
-            up = curr + helper1(board, i-1, j, memo, ret);
-            if (up > ret[0]) {
-                ret[0] = up;
-                ret[1] = 1;
-            } else if (up == ret[0]) {
-                ret[1]++;
-            }
+            up = curr + helper1(board, i-1, j, memo, nums);
         }
         if (i-1 >= 0 && j-1 >= 0 && board[i-1][j-1] != 'X') {
-            up_left = curr + helper1(board, i-1, j-1, memo, ret);
-            if (up_left > ret[0]) {
-                ret[0] = up_left;
-                ret[1] = 1;
-            } else if (up_left == ret[0]) {
-                ret[1]++;
-            }
+            up_left = curr + helper1(board, i-1, j-1, memo, nums);
         }
         if (j-1 >= 0 && board[i][j-1] != 'X') {
-            left = curr + helper1(board, i, j-1, memo, ret);
-            if (left > ret[0]) {
-                ret[0] = left;
-                ret[1] = 1;
-            } else if (left == ret[0]) {
-                ret[1]++;
+            left = curr + helper1(board, i, j-1, memo, nums);
+        }
+
+        if (up > -1) {
+            if (up == memo[i][j]) {
+                nums[i][j] += nums[i-1][j];
+            } else if (up > memo[i][j]) {
+                memo[i][j] = up;
+                nums[i][j] = nums[i-1][j];
             }
         }
-        memo[i][j] = max(up, max(up_left, left));
+        if (up_left > -1) {
+            if (up_left == memo[i][j]) {
+                nums[i][j] += nums[i-1][j-1];
+            } else if (up_left > memo[i][j]) {
+                memo[i][j] = up_left;
+                nums[i][j] = nums[i-1][j-1];
+            }
+        }
+        if (left > -1) {
+            if (left == memo[i][j]) {
+                nums[i][j] += nums[i][j-1];
+            } else if (left > memo[i][j]) {
+                memo[i][j] = left;
+                nums[i][j] = nums[i][j-1];
+            }
+        }
+
+        memo[i][j] = memo[i][j] % MOD;
+        nums[i][j] = nums[i][j] % MOD;
 
         return memo[i][j];
     }
--- cpp/1316.cpp
+++ cpp/1316.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int distinctEchoSubstrings(string text) {
         // return func1(text);
-        return func2(text);
+        // return func2(text);
+        return func3(text);
     }
 
     // TLE
@@ -42,7 +43,7 @@ public:
         }
         return ret;
     }
-    
+    // sometime AC, sometime TLE, slow
     int func2(string text) {
         int N = text.length();
         unordered_map<string, int> map;
@@ -53,19 +54,56 @@ public:
 
         for (int len = N/2; len >= 1; len--) {
             for (int i = 0; i+2*len-1 < N; i++) {
+                string s = text.substr(i, len);
+                if (map.find(s) != map.end()) {
+                    continue;
+                }
+
                 bool same = true;
                 for (int j = 0; j < len; j++) {
-                    if (text[j] != text[j+len]) {
+                    if (text[i+j] != text[i+j+len]) {
                         same = false;
                         break;
                     }
                 }
                 if (same) {
-                    map[text.substr(i, len)]++;
+                    map[s]++;
                 }
             }
         }
 
         return map.size();
     }
+    // TLE also
+    int func3(string text) {
+        int N = text.length();
+        if (N <= 0) {
+            return 0;
+        }
+        vector<vector<bool>> result(N, vector<bool>(N, false));
+        
+        for (int len = 1; len <= N/2; len++) {
+            for (int i = 0; i+2*len-1 < N; i++) {
+                bool same = true;
+                for (int j = 0; j < len; j++) {
+                    if (text[i+j] != text[i+j+len]) {
+                        same = false;
+                        break;
+                    }
+                }
+                result[i][i+2*len-1] = same;
+            }
+        }
+        unordered_set<string> set;
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                if (result[i][j]) {
+                    set.emplace(text.substr(i, j-i+1));
+                }
+            }
+        }
+        return set.size();
+    }
+
+    // MUST use hashing to compare string
 };
