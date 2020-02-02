6a9061f80e7f45d1ded94c27cfb8eae6af682743
xfmeng17 <xfmeng17@foxmail.com>
2020-02-02 23:50:09 +0800

    feat(cpp): unfinish weekly contest 174

---
+++ cpp/1342.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        vector<int> set(100001, 0);
+        for (auto a : arr) {
+            set[a]++;
+        }
+        
+        sort(set.begin(), set.end(), [](auto a, auto b) {
+            return a > b;
+        });
+        
+        int target = arr.size() / 2;
+        int i = 0;
+        while (target > 0) {
+            target -= set[i++];
+        }
+        
+        return i;
+    }
+};
