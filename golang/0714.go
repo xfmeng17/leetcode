93136830edc56bc9de4c7e68727a2512ac6c91ae
xfmeng17 <xfmeng17@foxmail.com>
2020-02-04 11:38:10 +0800

    feat(golang/0174): recode with series-of-stock problems solution

--- golang/0714.go
+++ golang/0714.go
@@ -1,5 +1,6 @@
 func maxProfit(prices []int, fee int) int {
-	return func4(prices, fee)
+	// return func4(prices, fee);
+	return func5(prices, fee);
 }
 
 // Copy from 309: Best Time to Buy and Sell Stock with Cooldown
@@ -23,6 +24,30 @@ func func4(prices []int, fee int) int {
 	}
 
 	return memo[n-1];
+}
+
+/*
+ * The series-of-stock problems
+ * T[i][0] = max(T[i-1][0], T[i-1][1] + prices[i])
+ * T[i][1] = max(T[i-1][1], T[i-1][0] - prices[i] - fee)
+ */
+const INT_MAX = int(^uint(0) >> 1);
+const INT_MIN = ^INT_MAX;
+func func5(prices []int, fee int) int {
+	if len(prices) <= 0 {
+		return 0;
+	}
+
+	T_ik0 := 0;
+	T_ik1 := INT_MIN;
+
+	for i := 0; i < len(prices); i++ {
+		T_ik0_aux := T_ik0;
+		T_ik0 = max(T_ik0, T_ik1 + prices[i]);
+		T_ik1 = max(T_ik1, T_ik0_aux - prices[i] - fee);
+	}
+
+	return T_ik0;
 }
 
 // Helper functions
