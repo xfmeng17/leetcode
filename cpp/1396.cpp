e0e78078bf69e07b7ad8c7244ccbb355c4501676
xfmeng17 <xfmeng17@foxmail.com>
2020-03-29 13:15:06 +0800

    feat(cpp): weekly-contest-182

---
+++ cpp/1396.cpp
@@ -0,0 +1,45 @@
+class UndergroundSystem {
+public:
+	unordered_map<int, pair<string, int>> check_map;
+    unordered_map<string, vector<int>> station_record;
+public:
+    UndergroundSystem() {
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        check_map[id] = make_pair(stationName, t);
+        return;
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        pair<string, int>& p = check_map[id];
+        string& st_station = p.first;
+        int st_time = p.second;
+        int costtime = t - st_time;
+        station_record[st_station + "_" + stationName].emplace_back(costtime);
+        check_map.erase(id);
+        return;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        double ave = 0.0;
+        double all = 0.0;
+        vector<int>& time_vec = station_record[startStation + "_" + endStation];
+      	if (time_vec.size() <= 0) {
+      		return 0.0;
+      	}
+
+        for (auto t : time_vec) {
+        	all += t + 0.0;
+        }
+        return all / (double)(time_vec.size());
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
---
+++ cpp/1397.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+     	return func1(s1, s2, evil);   
+    }
+
+    int func1(string s1, string s2, string evil) {
+    	int ret = 0;
+        if (s1.find(evil) == string::npos) {
+            ret++;
+        }
+    	while (nextString(s1, s2, s1.size()-1)) {
+    		if (s1.find(evil) == string::npos) {
+    			ret++;
+    		}
+    		// cout << s1 << endl;
+    	}
+    	return ret;
+    }
+
+    bool nextString(string& s1, string& s2, int pos) {
+    	if (pos < 0) {
+    		return false;
+    	}
+    	if (s1 == s2) {
+    		return false;
+    	}
+
+    	if (s1[pos] < 'z') {
+    		s1[pos] += 1;
+    		return true;
+    	}
+
+    	s1[pos] = 'a';
+    	pos -= 1;
+    	
+    	return nextString(s1, s2, pos);
+    }
+};
